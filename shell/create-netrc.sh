851b0b6d0cfe84e986548fd596a9b2f18413fefd
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-04-07 10:00:12 -0700

    Add lf ci-management jjb jobs
    This patch ports the jjb ci-management jobs over from OpenDaylight and
    makes necessary adjustments to make it generically re-useable as
    possible.
    
    This patch requires some environment variables defined. For example:
    
    JENKINS_HOSTNAME=jenkins092
    LOGS_SERVER=https://logs.opendaylight.org
    NEXUS_URL=https://nexus.opendaylight.org
    SILO=releng
    
    Issue: RELENG-32
    Change-Id: Ic19c5fa652885270d8e16992e82e642af95b0f4d
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/create-netrc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+NEXUS_URL="${NEXUS_URL:-$NEXUSPROXY}"
+CREDENTIAL=$(xmlstarlet sel -N "x=http://maven.apache.org/SETTINGS/1.0.0" \
+    -t -m "/x:settings/x:servers/x:server[x:id='${SERVER_ID}']" \
+    -v x:username -o ":" -v x:password \
+    "$SETTINGS_FILE")
+
+machine=$(echo "$NEXUS_URL" | awk -F/ '{print $3}')
+user=$(echo "$CREDENTIAL" | cut -f1 -d:)
+pass=$(echo "$CREDENTIAL" | cut -f2 -d:)
+
+echo "machine $machine login $user password $pass" > ~/.netrc
