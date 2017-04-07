5e5ea82ea7e068753084a37c3185c81e367060b5
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-04-07 10:00:12 -0700

    Add lf ci-management jjb jobs
    This patch ports the jjb ci-management jobs over from OpenDaylight and
    makes necessary adjustments to make it generically re-useable as
    possible.
    
    Issue: RELENG-32
    Change-Id: Ic19c5fa652885270d8e16992e82e642af95b0f4d
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/jjb-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> jjb-install.sh"
+
+virtualenv "$WORKSPACE/.virtualenvs/jjb"
+# shellcheck source=./.virtualenvs/jjb/bin/activate
+source "$WORKSPACE/.virtualenvs/jjb/bin/activate"
+pip install --upgrade pip
+pip install --upgrade "jenkins-job-builder==$JJB_VERSION"
+pip freeze
