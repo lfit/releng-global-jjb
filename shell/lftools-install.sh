f51ea89751c60fd1d213ed9829a7485d7aab87f4
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-04-07 10:00:12 -0700

    Add lf ci-management jjb jobs
    This patch ports the jjb ci-management jobs over from OpenDaylight and
    makes necessary adjustments to make it generically re-useable as
    possible.
    
    This patch requires some environment variables defined. For example:
    
    JENKINS_HOSTNAME=jenkins092
    LOGS_SERVER=https://logs.opendaylight.org
    NEXUS_URL=https://nexus.opendaylight.org
    SILO=releng
    
    Issue: RELENG-32
    Change-Id: Ic19c5fa652885270d8e16992e82e642af95b0f4d
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/lftools-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> lftools-install.sh"
+
+virtualenv "$WORKSPACE/.virtualenvs/lftools"
+# shellcheck source=./.virtualenvs/lftools/bin/activate disable=SC1091
+source "$WORKSPACE/.virtualenvs/lftools/bin/activate"
+pip install --upgrade pip
+pip install --upgrade "lftools<1.0.0"
+pip freeze
