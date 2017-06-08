0c236a4f32eb6d963eb47272af5991a64c545905
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-06-07 21:09:51 -0400

    Create Maven release job
    The Maven release job runs a build and deploys the artifacts into a
    local staging repository located at $WORKSPACE/m2repo which is then
    used to push to a Nexus staging repository later in the build process.
    
    - Adds a new Maven release job
    - Merges the maven-exec macro into a single lf-infra-maven-parameters
      macro.
    - Creates a common-variables.sh which can be sourced for useful common
      variables.
    
    Change-Id: I020d819ea59b809d06803c0dfd999ef002b201c4
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/maven-patch-release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+lftools version release PREPARE-RELEASE
+
+# Remove the release tag as it is not needed.
+find . -name "*.xml" | xargs sed -i 's/-PREPARE-RELEASE//g'
