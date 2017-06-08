34ab3807fb760dea4fdfc08b1692d49a243759f1
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-06-07 21:09:51 -0400

    Create Maven release job
    The Maven release job runs a build and deploys the artifacts into a
    local staging repository located at $WORKSPACE/m2repo which is then
    used to push to a Nexus staging repository later in the build process.
    
    - Adds a new Maven release job
    - Merges the maven-exec macro into a single lf-infra-maven-parameters
      macro.
    - Creates a common-variables.sh which can be sourced for useful common
      variables.
    - Store staging-repo number in archives staging-repo.txt for easy
      retrieval.
    
    Change-Id: I020d819ea59b809d06803c0dfd999ef002b201c4
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/maven-build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# This script builds a Maven project and deploys it into a staging repo which
+# can be used to deploy elsewhere later eg. Nexus staging / snapshot repos.
+
+# DO NOT enable -u because $MAVEN_PARAMS and $MAVEN_OPTIONS could be unbound.
+# Ensure we fail the job if any steps fail.
+set -e -o pipefail
+set +u
+
+export MAVEN_OPTS
+
+# Disable SC2086 because we want to allow word splitting for $MAVEN_* parameters.
+# shellcheck disable=SC2086
+$MVN clean deploy \
+    --global-settings "$GLOBAL_SETTINGS_FILE" \
+    --settings "$SETTINGS_FILE" \
+    -DaltDeploymentRepository=staging::default::file:"$WORKSPACE"/m2repo \
+    $MAVEN_PARAMS $MAVEN_OPTIONS
---
+++ shell/maven-stage.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# This script publishes artifacts to a staging repo in Nexus.
+#
+# $WORKSPACE/m2repo   :  Exists and used to deploy the staging repository.
+# $NEXUS_URL          :  Jenkins global variable should be defined.
+# $STAGING_PROFILE_ID :  Provided by a job parameter.
+
+# Ensure we fail the job if any steps fail.
+set -eu -o pipefail
+
+TMP_FILE="$(mktemp)"
+lftools deploy nexus-stage "$NEXUS_URL" "$STAGING_PROFILE_ID" "$WORKSPACE/m2repo" | tee "$TMP_FILE"
+staging_repo=$(sed -n -e 's/Staging repository \(.*\) created\./\1/p' "$TMP_FILE")
+
+# Store repo info to a file in archives
+mkdir -p "$WORKSPACE/archives"
+echo "$staging_repo" > "$WORSPACE/archives/staging-repo.txt"
+
+# Cleanup
+rm "$TMP_FILE"
