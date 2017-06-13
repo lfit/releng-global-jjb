5c4003381da43efe2777cbd0fcc3c64fb3ce0802
Jessica Wagantall <jwagantall@linuxfoundation.org>
2017-06-13 10:43:39 -0700

    lf-ci-jobs: Add check_no_jira_urls.sh
    Across projects, we have commit messages being
    merged with jira URLs instead of the accepted
    format: Issue: <JIRAKEY>-<ISSUE#>
    Jira URLs break the its-jira plugin making the
    link unaccessible from gerrit to the jira issue.
    
    Change-Id: Iecc82b34275d640549f974312bfba65fe3fa56f7
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

---
+++ shell/git_validate_jira_urls.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# This script will make sure that there are no JIRA URLs in the commit
+# message. JIRA URLs will break the its-jira plugin
+
+set -e -o pipefail
+set +u
+
+JIRA_LINK=`git rev-list --format=%B --max-count=1 HEAD | grep -io 'http[s]*://jira\..*'`
+if [[ ! -z "$JIRA_LINK" ]]
+then
+  echo 'Remove JIRA URLs from commit message'
+  echo 'Add jira references as: Issue: <JIRAKEY>-<ISSUE#>, instead of URLs'
+  exit 1
+fi
