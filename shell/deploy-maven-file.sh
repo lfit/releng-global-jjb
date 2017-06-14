d2401a365852bb6099e598af39bcc7a3f2baae6e
Anil Belur <abelur@linuxfoundation.org>
2017-06-14 18:12:40 +1000

    Add maven-deploy-file builder for packaging jobs
    Change-Id: I243d13c257756f653b702c57e5d8c700411c53e9
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

---
+++ shell/deploy-maven-file.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# This script publishes packages (rpms/debs) or any file to Nexus hosted
+# maven2 repository.
+#
+# $NEXUS_URL          :  Jenkins global variable should be defined.
+# $REPO_ID            :  Provided by a job parameter.
+# $GROUP_ID           :  Provided by a job parameter.
+# $UPLOAD_FILES_PATH  :  Provided by a job parameter.
+echo "---> deploy-maven-file.sh"
+# Ensure we fail the job if any steps fail.
+set -eu -o pipefail
+
+DEPLOY_LOG="$WORKSPACE/archives/deploy-maven-file.log"
+mkdir -p "$WORKSPACE/archives"
+
+NEXUS_URL_REPOID="${NEXUS_URL}/${REPO_ID}"
+mkdir -p "$UPLOAD_FILES_PATH"
+
+while IFS="" read -r file
+do
+    lftools deploy maven-file "$NEXUS_URL" \
+                              "$REPO_ID" \
+                              "$file" \
+                              "-b $MVN" \
+                              "-g $GROUP_ID" | tee "$DEPLOY_LOG"
+done < <(find "$UPLOAD_FILES_PATH" -type d -name "*")
