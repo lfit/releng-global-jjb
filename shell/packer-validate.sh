cee7f3b96e19581594510c6bc4bb61a68b939b36
Anil Belur <abelur@linuxfoundation.org>
2017-07-03 21:54:03 +1000

    Add generic packer templates
    - Packer jobs are designed to validate packer files and build
      custom images. There templates can be used for LF projects which
      includes global-jjb.
    - When Packer binaries are not installed on the build node
      the pulls in packer before running a packer build.
    - Update test templates according to new packer jobs introduced.
    
    Change-Id: I3657621e30d402655b84973ea4b4c48fa21cea2d
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

---
+++ shell/packer-validate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# @SPDX-License-Identifier EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> packer-validate.sh"
+# The script validates an packers files.
+
+# $CLOUDENV            :  Provides the cloud credential file.
+
+# Ensure we fail the job if any steps fail.
+set -eu -o pipefail
+
+PACKER_LOGS_DIR="$WORKSPACE/archives/packer"
+mkdir -p "$PACKER_LOGS_DIR"
+
+cd packer
+varfiles=(../packer/vars/*)
+templates=(../packer/templates/*)
+
+for varfile in "${varfiles[@]}"; do
+    [[ "${varfile##*/}" =~ ^(cloud-env.*)$ ]] && continue
+    for template in "${templates[@]}"; do
+        export PACKER_LOG="yes" && \
+        export PACKER_LOG_PATH="$PACKER_LOGS_DIR/packer-validate-${varfile##*/}-${template##*/}.log" && \
+                    packer.io validate -var-file="$CLOUDENV" \
+                    -var-file="$varfile" "$template"
+        if [ $? -ne 0 ]; then
+            break
+        fi
+    done
+done
