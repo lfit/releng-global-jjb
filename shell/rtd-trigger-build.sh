ff9d182eb5ddd6d1b4b7e15af142aa49cd0d9ab1
Jeremy Phelps <jphelps@linuxfoundation.org>
2017-07-26 16:25:05 -0500

    Generalize odl rtd jobs for global-jjb
    Change-Id: I35bdd1629c4f1079131048a8a8f41f09949796ff
    JIRA: RELENG-240
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

---
+++ shell/rtd-trigger-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+
+if [ "$GERRIT_BRANCH" == "master" ]; then
+    RTD_BUILD_VERSION=latest
+else
+    RTD_BUILD_VERSION="${GERRIT_BRANCH/\//-}"
+fi
+
+# shellcheck disable=SC1083
+curl -X POST --data "version_slug=$RTD_BUILD_VERSION" https://readthedocs.org/build/$RTD_PROJECT
