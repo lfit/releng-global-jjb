8f4dea21dfa2e3cf91df567e1511936c9711722f
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-08-30 17:56:55 -0400

    Create job to lock|unlock branches via Gerrit
    A contributor can leave a comment "lock|unlock branch" in Gerrit comment
    which triggers a job to submit a patch to Gerrit for the project to lock
    or unlock their branch.
    
    Locking refers to removing the permission to click the Submit button in
    the project's branch effectively disallowing new patches to be merged
    into the repo. This job simply generates and submits a patch. A project
    committer still needs to approve it.
    
    Issue: RELENG-424
    Change-Id: Id489d931037c76bdcad032ec44b0ce748bdffe81
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/gerrit-branch-lock.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+# Generates a patch to lock|unlock a branch in Gerrit
+
+# Ensure we fail the job if any steps fail.
+set -eu -o pipefail
+
+git fetch origin refs/meta/config:config
+git checkout config
+
+# Install Gerrit Hook
+curl -Lo .git/hooks/commit-msg "http://$(git remote show origin | grep Fetch | awk -F'/' '{print $3}' | awk -F'@' '{print $NF}' | awk -F':' '{print $1}')/tools/hooks/commit-msg"
+chmod u+x .git/hooks/commit-msg
+
+mode=$(echo "$GERRIT_EVENT_COMMENT_TEXT" | grep branch | awk '{print $1}')
+case $mode in
+    lock)
+        echo "Locking branch: $GERRIT_BRANCH"
+        git config -f project.config "access.refs/heads/${GERRIT_BRANCH}.exclusiveGroupPermissions" "submit"
+        git config -f project.config "access.refs/heads/${GERRIT_BRANCH}.submit" "block group Registered Users"
+        git commit -asm "Lock branch $GERRIT_BRANCH"
+        ;;
+    unlock)
+        echo "Unlocking branch: $GERRIT_BRANCH"
+        git config -f project.config --remove-section "access.refs/heads/${GERRIT_BRANCH}" || true
+        git commit -asm "Unlock branch $GERRIT_BRANCH"
+        ;;
+    *)
+        echo "Unknown mode selected: $mode"
+        ;;
+esac
+
+git diff HEAD~1
+git push origin HEAD:refs/for/refs/meta/config
