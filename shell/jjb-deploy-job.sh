88f2843f58692701e1212e5b18c0dad8c1e29329
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-09-07 21:40:56 -0400

    Add job to deploy to sandbox
    This job allows a contributor to leave a comment for example
    'jjb-deploy builder-jjb-*' to deploy a job (or several jobs if wildcards
    are used) to the Jenkins Sandbox.
    
    This allows jjb contributors to no longer have to install jjb on their
    local systems in order to push the job to the Jenkins Sandbox.
    
    ATTN: GitHub version of the job is untested and likely does NOT work as
          currently designed.
    
    Issue: RELENG-373
    Change-Id: I4f26c640804a0b7db1a0087cbd805b9263821326
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/jjb-deploy-job.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+# Push a job to jenkins-sandbox via Gerrit / GitHub comment
+# Comment Pattern: jjb-deploy JOB_NAME
+# JOB_NAME: Can also include * wildcards too. Additional parameters are ignored.
+echo "---> jjb-deploy-job.sh"
+
+# Ensure we fail the job if any steps fail.
+set -e -o pipefail
+
+# shellcheck source=/tmp/v/jenkins-job-builder/bin/activate disable=SC1091
+source "/tmp/v/jenkins-job-builder/bin/activate"
+
+echo "-----> Fetching project"
+git fetch origin "$GERRIT_REFSPEC" && git checkout FETCH_HEAD
+
+# If not Gerrit Trigger than assume GitHub
+COMMENT="${GERRIT_EVENT_COMMENT_TEXT:-$ghprbCommentBody}"
+JOB_NAME=$(echo "$COMMENT" | grep jjb-deploy | awk '{print $2}')
+
+jenkins-jobs update --recursive --workers 4 jjb/ "$JOB_NAME"
