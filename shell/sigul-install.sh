53f9ec68cdfd3418281fb63b789c569110ed391e
Andrew Grimberg <agrimberg@linuxfoundation.org>
2017-09-22 07:23:19 -0700

    Base sigul macros and scripts
    Before any jobs can be created using sigul we need to have some basic
    scaffolding for setting up the sigul enviroment.
    
    Changes to lftools to extend the signing command to leverage sigul will
    then allow us to create a meta macro which combines this scaffolding
    into a single cohesive macro.
    
    Change-Id: I080c1eb7271a2a64559b2c2f7b1011be1e9a8b16
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

---
+++ shell/sigul-install.sh
@@ -0,0 +1,41 @@
+/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> sigul-install.sh"
+
+# Ensure we fail the job if any steps fail.
+set -eu -o pipefail
+
+# Setup sigul RPM repo
+echo "[fedora-infra-sigul]
+name=Fedora builder packages for sigul
+baseurl=https://infrastructure.fedoraproject.org/repo/infra/epel\$releasever-infra/\$basearch/
+enabled=1
+gpgcheck=1
+gpgkey=https://infrastructure.fedoraproject.org/repo/infra/RPM-GPG-KEY-INFRA-TAGS
+includepkgs=sigul*
+skip_if_unavailable=True" > fedora-infra-sigul.repo
+
+sudo cp fedora-infra-sigul.repo /etc/yum.repos.d
+rm fedora-infra-sigul.repo
+
+# install sigul
+sudo yum install -y -q sigul
+
+# configure /etc/hosts with the sigul bridge hostname
+# This is needed as build minions can't always get DNS resolution
+# on the bridge
+BRIDGE_HOST=$(grep bridge-hostname "$SIGUL_CONFIG" | awk '{print $2}')
+
+# SIGUL_BRIDGE_IP must be defined as a Jenkins env variable
+cp /etc/hosts hosts
+echo -e "$SIGUL_BRIDGE_IP\t$BRIDGE_HOST" >> hosts
+sudo cp hosts /etc/hosts
+rm hosts
