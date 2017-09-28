32c3ebc132d965f801999b3270939b5fec8935b5
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-09-28 11:26:37 -0400

    Add license check job using zxiiro/lhc
    Create a job using https://github.com/zxiiro/license-header-checker as
    the license checker tool for projects.
    
    Start with only a Gerrit job for now as the GitHub job will need to be
    designed in a different way as there's no way to make a single job that
    watches every github project. We'll likely need to think a bit more
    about how to enforce this on the github side as we do not want the
    configuration to be managed by the project themselves.
    
    Change-Id: I5ca6da61dc6ee250a2c6d451b09a0075fb1ff0b0
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/license-check.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> license-check.sh"
+
+# Ensure we fail the job if any steps fail.
+set -eu -o pipefail
+
+if hash lhc 2>/dev/null; then
+    echo "License Header Checker is installed."
+    lhc --version
+else
+    echo "License Header Checker is not installed. Installing..."
+    mkdir "$WORKSPACE/bin"
+    # TODO: Update this once we have a more permanent location for the project.
+    wget -nv -O "$WORKSPACE/bin/lhc" https://github.com/zxiiro/license-header-checker/releases/download/v0.1.0/lhc
+    chmod +x "$WORKSPACE/bin/lhc"
+    export PATH="$WORKSPACE/bin:$PATH"
+    lhc --version
+fi
+
+lhc --license Apache-2.0,EPL-1.0,MIT \
+    '*.go' \
+    '*.java' \
+    '*.py' \
+    '*.sh'
