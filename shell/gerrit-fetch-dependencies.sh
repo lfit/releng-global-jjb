f82d276dda43c38002a0f0bec43df1aff0166403
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-10-12 23:34:15 -0400

    Add Maven job to build dependencies with patches
    Provide a Maven job that can verify a patch including firstly building
    projects it depends on with their patches first. This allows projects to
    validate a patch that has upstream project dependencies which need to be
    merged first.
    
    Requires a new global variable to be defined in Jenkins GERRIT_URL
    containing the base path to the Gerrit Web UI.
    
    Issue: RELENG-526
    Change-Id: Ie9ccb88c21ab5674d0156566da1daee4aa43d641
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/gerrit-fetch-dependencies.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+# Fetches patches all projects provided by comment trigger
+#
+# Takes a list of Gerrit patches and fetches all projects and cherry-pick
+# patches for projects. The trigger is
+#     'recheck: SPACE_SEPERATED_LIST_OF_PATCHES'
+#
+# NOTE: This script assumes the user will provide the correct dependency order
+#       via the PATCHES list.
+
+# Ensure we fail the job if any steps fail.
+set -eu -o pipefail
+
+REPOS_DIR="$WORKSPACE/.repos"
+
+PATCHES=($(echo "$GERRIT_EVENT_COMMENT_TEXT" | grep 'recheck:' | awk -F: '{print $2}'))
+
+projects=()
+for patch in $(echo "${PATCHES[@]}"); do
+    json=$(curl -s "$GERRIT_URL/changes/$patch" | sed -e "s/)]}'//")
+    project=$(echo "$json" | jq -r '.project')
+    branch=$(echo "$json" | jq -r '.branch')
+
+    if [ ! -d "$REPOS_DIR/$project" ]; then
+        git clone -q --depth 1 "$GIT_URL/$project" "$REPOS_DIR/$project"
+
+        # This array will be used later to determine project build order.
+        projects+=("$project")
+    fi
+
+    pushd "$REPOS_DIR/$project"
+    git remote add gerrit "$GIT_URL/$project"
+    git review --cherrypick="$patch"
+    popd
+done
+
+# This script should be a macro which re-inject's the projects variable back
+# into the build so a script later on can use it.
+echo "DEPENDENCY_BUILD_ORDER=${projects[@]}" > "$WORKSPACE/.dependency.properties"
---
+++ shell/maven-build-deps.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# Builds projects provided via $DEPENDENCY_BUILD_ORDER list
+#
+# This runs a `mvn clean install` against all projects provided by a list. This
+# script is a companion script for the gerrit-fetch-dependencies script which
+# clones project repos to "$WORKSPACE/.repos".
+
+# DO NOT enable -u because $MAVEN_PARAMS and $MAVEN_OPTIONS could be unbound.
+# Ensure we fail the job if any steps fail.
+set -e -o pipefail
+set +u
+
+PROJECTS=($(echo "$DEPENDENCY_BUILD_ORDER"))
+REPOS_DIR="$WORKSPACE/.repos"
+
+export MAVEN_OPTS
+
+for project in "$PROJECTS"; do
+    pushd "$REPOS_DIR/$project"
+    # Disable SC2086 because we want to allow word splitting for $MAVEN_* parameters.
+    # shellcheck disable=SC2086
+    $MVN clean install \
+        -Pq \
+        -DskipTests=true \
+        --global-settings "$GLOBAL_SETTINGS_FILE" \
+        --settings "$SETTINGS_FILE" \
+        $MAVEN_PARAMS $MAVEN_OPTIONS
+    popd
+done
