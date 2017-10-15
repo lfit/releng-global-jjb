e8946cb7fcbb5c12322af634eaf8244d4cb524af
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-10-15 14:15:26 -0400

    Release global-jjb v0.10.0
    Change-Id: If8918550d84c703a6704b0e955e0a430bcb74250
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ author = 'Linux Foundation Releng'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.10.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
