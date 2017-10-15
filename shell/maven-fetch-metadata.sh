d8062dece355de7a69d3fe351bf2bbda1ed0a891
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-10-15 16:44:08 -0400

    Migrate Maven Merge job to global-jjb
    Provide a Maven Merge job that is generically reusable by other
    projects. This new job uses cURL to upload artifacts to Nexus and uses
    `wget -r` to mirror existing maven-metadata.xml files in order to update
    them when pushing SNAPSHOTs.
    
    This job uses the following strategy to deploy jobs to Nexus:
    
    1. `wget -r` to fetch maven-metadata.xml from Nexus
    2. `mvn deploy -DaltDeploymentRepository` to prepare files for upload
    3. Removes untouched maven-metadata.xml files before upload
    4. Use lftools (cURL) upload script to push artifacts to Nexus
    
    Issue: RELENG-61
    Change-Id: I7663731d0eb750e4ccc1b07bcf5ad39c6135900c
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/maven-fetch-metadata.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# Uses wget to fetch a project's maven-metadata.xml files from a Maven repository.
+
+# DO NOT enable -u because $MAVEN_PARAMS and $MAVEN_OPTIONS could be unbound.
+# Ensure we fail the job if any steps fail.
+set -e -o pipefail
+set +u
+
+project=$(xmlstarlet sel \
+    -N "x=http://maven.apache.org/POM/4.0.0" -t \
+    --if "/x:project/x:groupId" \
+      -v "/x:project/x:groupId" \
+    --elif "/x:project/x:parent/x:groupId" \
+      -v "/x:project/x:parent/x:groupId" \
+    --else -o "" pom.xml)
+project_path="${project//.//}"
+
+mkdir -p "$WORKSPACE/m2repo/$project_path"
+pushd "$WORKSPACE/m2repo/$project_path"
+
+    wget -nv --recursive \
+         --accept maven-metadata.xml \
+         -R "index.html*" \
+         --execute robots=off \
+         --no-parent \
+         --no-host-directories \
+         --cut-dirs="$NEXUS_CUT_DIRS" \
+         "$NEXUS_URL/content/repositories/$NEXUS_REPO/$project_path/"
+
+popd
+
+# Backup metadata - Used later to find metadata files that have not been modified
+mkdir -p "$WORKSPACE/m2repo-backup"
+cp -a "$WORKSPACE/m2repo/*" "$WORKSPACE/m2repo-backup"
