a0e994cd728249184b302885ad963c384cd5bb43
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-10-15 16:44:08 -0400

    Migrate Maven Merge job to global-jjb
    Provide a Maven Merge job that is generically reusable by other
    projects. This new job uses cURL to upload artifacts to Nexus and uses
    `wget -r` to mirror existing
    
    This job uses the following strategy to deploy jobs to Nexus:
    
    1. `wget -r` to fetch maven-metadata.xml from Nexus
    2. `mvn deploy -DaltDeploymentRepository to prepare files for upload
    3. Use lftools (cURL) upload script to push artifacts to Nexus
    
    Issue: RELENG-61
    Change-Id: I7663731d0eb750e4ccc1b07bcf5ad39c6135900c
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/maven-deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# Deploy maven artifacts to Nexus using cURL
+
+# DO NOT enable -u because $MAVEN_PARAMS and $MAVEN_OPTIONS could be unbound.
+# Ensure we fail the job if any steps fail.
+set -e -o pipefail
+set +u
+
+m2repo_dir="$WORKSPACE/m2repo"
+
+# Remove metadata files that were not updated.
+metadata_files=($(diff -s -r "$m2repo_dir" "$WORKSPACE/m2repo-backup" \
+    | grep 'Files .* and .* are identical' | awk '{print $2}'))
+
+for i in "${metadata_files[@]}"; do
+    rm "$i*"
+done
+
+find "$m2repo_dir" -type d -empty -delete
