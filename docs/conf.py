fc75b20f7ec19cb7cfdbd74385bf1262ccb7a84c
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-10-30 12:30:17 -0400

    Bump to v0.12.0-dev
    Change-Id: I5eaaa3d84e66a0216225c9b4a16baedf4393d461
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ author = 'Linux Foundation Releng'
 # built documents.
 #
 # The short X.Y version.
-version = '0.11.0'
+version = '0.12.0-dev'
 # The full version, including alpha/beta/rc tags.
 release = version
 
@@ -258,4 +258,3 @@ intersphinx_mapping = {
     'lftools': ('http://lftools.releng.linuxfoundation.org/en/latest/', None),
     'python': ('https://docs.python.org/', None),
 }
-
