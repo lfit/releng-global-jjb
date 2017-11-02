56275528c1b8785d504a4174b110ecb3b9fab778
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-11-02 10:36:59 -0400

    Add install goal to javadoc job
    It turns out javadoc needs to have the artifacts available to them in
    order to run javadoc:aggregate. In some cases this means artifacts
    installed to the local maven repo. This patch adds the install goal as
    well as ensuring we continue to skip tests, checkstyle, and findbugs to
    keep the jobs running quick.
    
    Change-Id: I3ffa36b207a1412e0489d6c20a91ffcd3ac93c27
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- shell/maven-javadoc-generate.sh
+++ shell/maven-javadoc-generate.sh
@@ -23,7 +23,10 @@ export MAVEN_OPTS
 
 # Disable SC2086 because we want to allow word splitting for $MAVEN_* parameters.
 # shellcheck disable=SC2086
-$MVN clean javadoc:aggregate \
+$MVN clean install javadoc:aggregate \
+    -DskipTests=true \
+    -Dcheckstyle.skip=true \
+    -Dfindbugs.skip=true \
     --global-settings "$GLOBAL_SETTINGS_FILE" \
     --settings "$SETTINGS_FILE" \
     $MAVEN_PARAMS $MAVEN_OPTIONS
