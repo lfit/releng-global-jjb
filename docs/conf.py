5e403dec080850163f75cf80e0317fcf839d69aa
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-01-10 16:31:08 -0500

    Release global-jjb v0.14.1
    Change-Id: Ice69cad332a65f91db0d558130f6ae2d51767a57
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ author = 'Linux Foundation Releng'
 # built documents.
 #
 # The short X.Y version.
-version = '0.14.0'
+version = '0.14.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
