49afe0ed6fc5a89d3cdc5879216bb0aa8722f835
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-01-13 12:05:34 -0500

    Do not force expected-xml
    After using this for a few months now I'm convinced this is not the
    right way to achieve what we want here. XML differences should be
    informational and not build breaking. Pretty much every change
    requires XML changes so I think this has been more annoying than
    helpful.
    
    This new patch removes expected-xml and instead outputs XML diffs
    instead which can be stored in logs and reviewed.
    
    Change-Id: Ia4039489502499df1b15a4d9fe1e553af3c5bfcd
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb-compare-xml.sh
+++ jjb-compare-xml.sh
@@ -9,15 +9,27 @@
 # http://www.eclipse.org/legal/epl-v10.html
 ##############################################################################
 
-# This script tests jjb templates by comparing the result with expected output.
+# This script tests jjb templates by comparing the result with expected output
+# from global-jjb's origin/master branch.
 
 test_dir=$(mktemp -d)
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-expected_xml_dir="$script_dir/.jjb-test/expected-xml"
+expected_xml_dir="$(mktemp -d -t gjjb-XXXXXXXX)"
 
 echo "Script Directory: $script_dir"
 echo "Test Directory: $test_dir"
+echo "Expected XML Directory: $expected_xml_dir"
 
+git fetch origin
+gittmp="$(mktemp -d)"
+git worktree add --detach "$gittmp" origin/master
+pushd "$gittmp" || exit
+echo "Generating expected XML."
+jenkins-jobs test --recursive -o "$expected_xml_dir" "$gittmp":.jjb-test > /dev/null 2>&1
+popd || exit
+rm -r "$gittmp"
+
+echo "Generating test XML."
 jenkins-jobs test --recursive -o "$test_dir" "$script_dir":.jjb-test
 
 fail=false
@@ -31,9 +43,9 @@ for xml in "$test_dir"/*; do
 done
 
 # Cleanup
-rm -rf "$test_dir"
+rm -rf "$test_dir" "$expected_xml_dir"
 
 if $fail; then
-    echo "Differences detected. Check above for jobs that have been changed."
-    exit 1
+    echo "WARN: Differences detected. Check above for jobs that have been changed."
+    exit 0
 fi
