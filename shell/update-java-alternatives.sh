61fe47417032bde7833830f6c5a3ebb2a4c8fe45
Jessica Wagantall <jwagantall@linuxfoundation.org>
2018-01-23 16:31:47 -0800

    Add script to update Java alternatives
    Use ${java-version} to set the correct java, javac
    and java_sdk_openjdk alternative for lf-maven-jobs.
    
    Issue-ID: RELENG-752
    Change-Id: Iff0c2e7d92c4cc802925ae67cdda1f4247db52be
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

---
+++ shell/update-java-alternatives.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# This script takes the java-version variable to set the proper alternative
+# for java, javac and java_sdk_openjdk for ubuntu or centos/fedora/redhat distros
+
+JAVA_ENV_FILE="/tmp/java.env"
+
+update-java-redhat() {
+    export JAVA_HOME="/usr/lib/jvm/java-1.${SET_JDK_VERSION: -1}.0-openjdk"
+    sudo /usr/sbin/alternatives --install /usr/bin/java java "${JAVA_HOME}/bin/java" 1
+    sudo /usr/sbin/alternatives --install /usr/bin/javac javac "${JAVA_HOME}/bin/javac" 1
+    sudo /usr/sbin/alternatives --install /usr/lib/jvm/java-openjdk java_sdk_openjdk "${JAVA_HOME}" 1
+    sudo /usr/sbin/alternatives --set java "${JAVA_HOME}/bin/java"
+    sudo /usr/sbin/alternatives --set javac "${JAVA_HOME}/bin/javac"
+    sudo /usr/sbin/alternatives --set java_sdk_openjdk "${JAVA_HOME}"
+    echo JAVA_HOME="$JAVA_HOME" > "$JAVA_ENV_FILE"
+}
+
+update-java-ubuntu() {
+    export JAVA_HOME="/usr/lib/jvm/java-${SET_JDK_VERSION: -1}-openjdk-amd64"
+    sudo /usr/bin/update-alternatives --install /usr/bin/java java "${JAVA_HOME}/bin/java" 1
+    sudo /usr/bin/update-alternatives --install /usr/bin/javac javac "${JAVA_HOME}/bin/javac" 1
+    sudo /usr/bin/update-alternatives --install /usr/lib/jvm/java-openjdk java_sdk_openjdk "${JAVA_HOME}" 1
+    sudo /usr/bin/update-alternatives --set java "${JAVA_HOME}/bin/java"
+    sudo /usr/bin/update-alternatives --set javac "${JAVA_HOME}/bin/javac"
+    sudo /usr/bin/update-alternatives --set java_sdk_openjdk "${JAVA_HOME}"
+    echo JAVA_HOME="$JAVA_HOME" > "$JAVA_ENV_FILE"
+}
+
+echo "---> Updating Java version"
+OS=$(facter operatingsystem | tr '[:upper:]' '[:lower:]')
+
+case "${OS}" in
+    fedora|centos|redhat)
+        echo "---> RedHat type system detected"
+        update-java-redhat
+    ;;
+    ubuntu)
+        echo "---> Ubuntu system detected"
+        update-java-ubuntu
+    ;;
+esac
+java -version
+echo JAVA_HOME="${JAVA_HOME}"
