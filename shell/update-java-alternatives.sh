92593cb9e26e63efde8c6d1faedc0633130afc6b
Jessica Wagantall <jwagantall@linuxfoundation.org>
2018-01-23 16:31:47 -0800

    Add script to update Java alternatives
    Use ${java-version} to set the correct java, javac
    and java_sdk_openjdk alternative for lf-maven-jobs.
    
    Change-Id: Iff0c2e7d92c4cc802925ae67cdda1f4247db52be
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

---
+++ shell/update-java-alternatives.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SET_JDK_VERSION=${java-version}
+
+update-java-rh() {
+    export JAVA_HOME="/usr/lib/jvm/java-1.${SET_JDK_VERSION: -1}.0-openjdk"
+    sudo /usr/sbin/alternatives --set java ${JAVA_HOME}/bin/java
+    sudo /usr/sbin/alternatives --set javac ${JAVA_HOME}/bin/javac
+    sudo /usr/sbin/alternatives --set java_sdk_openjdk ${JAVA_HOME}
+    mvn --version
+    echo JAVA_HOME=${JAVA_HOME}
+}
+
+update-java-ubuntu() {
+    export JAVA_HOME="/usr/lib/jvm/java-${SET_JDK_VERSION: -1}-openjdk"
+    sudo /usr/sbin/alternatives --set java ${JAVA_HOME}/bin/java
+    sudo /usr/sbin/alternatives --set javac ${JAVA_HOME}/bin/javac
+    sudo /usr/sbin/alternatives --set java_sdk_openjdk ${JAVA_HOME}
+    mvn --version
+    echo JAVA_HOME=${JAVA_HOME}
+}
+
+echo "---> Updating JAVA version"
+OS=$(facter operatingsystem | tr '[:upper:]' '[:lower:]')
+
+case "${OS}" in
+    fedora|centos|redhat)
+        echo "---> RH type system detected"
+        update-java-rh
+    ;;
+    ubuntu)
+        echo "---> Ubuntu system detected"
+        update-java-ubuntu
+    ;;
+esac
