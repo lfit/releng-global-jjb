3ca3fb8e9d386adedaea211a0afefa5d19cce223
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-02-12 18:55:07 -0500

    Bump version to 0.17.0-dev
    Change-Id: I9c352b633fcf83a0b60e2351401cd76c42df238d
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ author = 'Linux Foundation Releng'
 # built documents.
 #
 # The short X.Y version.
-version = '0.16.0'
+version = '0.17.0-dev'
 # The full version, including alpha/beta/rc tags.
 release = version
 
