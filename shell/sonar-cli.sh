970332848a79a7c56de9843bb423c604503bd4ab
Jessica Wagantall <jwagantall@linuxfoundation.org>
2018-03-06 17:10:41 -0800

    Add CLM support for Python
    Add template to support CLM scans for python
    based projects. This template uses the nexus-iq-cli
    package directly and executes an extended report to
    scan python based projects.
    
    Change-Id: I5e5c420539ab3a3e5b292620da4418c16d557221
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

---
+++ shell/sonar-cli.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# This script downloads nexus-iq-cli-1.44.0-01.jar and uses it to perform an
+# XC Evaluation or extended report which provides a scan of python files within
+# the repo
+
+set +x
+wget https://download.sonatype.com/clm/scanner/${NEXUS_IQ_CLI_JAR}
+echo "-a" > cli-auth.txt
+echo "${CLM_USER}:${CLM_PASSWORD}" >> cli-auth.txt
+java -jar nexus-iq-cli-1.44.0-01.jar -xc -i ${CLM_PROJECT_NAME} -s https://nexus-iq.wl.linuxfoundation.org -t build .
