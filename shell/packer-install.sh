2ccc1f88646eb5f73b35604d550e7bbefed053ac
Anil Belur <abelur@linuxfoundation.org>
2018-03-26 15:03:25 -0700

    WIP: Check if packer version 1.1.3 is installed
    Packer install script only checks for binary available in the $PATH
    before updating the packer image and skips updating the version of packer
    if a older version is already available.
    Packer 1.1.3 is the minimum version required to be installed
    to build images on vexxhost.
    
    Change-Id: I8c343176d8f39e3e70b04dddbdec94ffed4354b1
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- shell/packer-install.sh
+++ shell/packer-install.sh
@@ -13,16 +13,37 @@ echo "---> packer-install.sh"
 # if its not available
 
 # $PACKER_VERSION        : Define a packer version passed as job paramter
-
 PACKER_VERSION="${PACKER_VERSION:-1.1.3}"
 
 # Ensure we fail the job if any steps fail.
 set -eu -o pipefail
 
+flag=1
+
+function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
+function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" == "$1"; }
+function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
+function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
+
 if hash packer.io 2>/dev/null; then
-    echo "packer.io command is available."
-else
-    echo "packer.io command not is available. Installing packer ..."
+    CURRENT_VERSION="$(packer.io --version)"
+    if version_gt $CURRENT_VERSION $PACKER_VERSION; then
+       echo "$CURRENT_VERSION is greater than $PACKER_VERSION"
+       flag=0
+    elif version_lt $CURRENT_VERSION $PACKER_VERSION; then
+       echo "$CURRENT_VERSION is less than $PACKER_VERSION"
+       flag=1
+    elif version_le $CURRENT_VERSION $PACKER_VERSION; then
+       echo "$CURRENT_VERSION is less than or equal to $PACKER_VERSION"
+       flag=0
+    elif version_ge $CURRENT_VERSION $PACKER_VERSION; then
+       echo "$CURRENT_VERSION is greater than or equal to $PACKER_VERSION"
+       flag=0
+    fi
+fi
+
+if [[ "$flag" != "0" ]]; then
+    echo "Installing packer ..."
     # Installs Hashicorp's Packer binary, required for verify & merge packer jobs
     pushd "${WORKSPACE}"
     wget -nv "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip"
