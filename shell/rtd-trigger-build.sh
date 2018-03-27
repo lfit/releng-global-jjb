fbc0cea04f5f5c2055d46516e68ea168ea328ed1
Jeremy Phelps <jphelps@linuxfoundation.org>
2018-03-27 11:45:57 -0500

    Update RTD to use Generic API v2 webhook
    The old tokenless generic trigger url has been deprecated. Update
    to using the new v2 API that requires a token.
    
    This is a backwards incompatible change and modifies 2 things:
    
      * rtd-project is now the rtd-build-url parameter
      * rtd-token is a new required parameter
    
    Issue: RELENG-879
    Change-Id: I786240ab14cf95a2896d92d1c87aef9d7815d964
    Co-Authored-By: Thanh Ha <thanh.ha@linuxfoundation.org>
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- shell/rtd-trigger-build.sh
+++ shell/rtd-trigger-build.sh
@@ -8,16 +8,19 @@
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
 ##############################################################################
+# Call cURL to trigger a build in RTD via the Generic API
+#
+# Paramters:
+#     RTD_BUILD_URL: The unique build URL for the project.
+#                    Check Admin > Integrations > Generic API incoming webhook.
+#
+#     RTD_TOKEN: The unique token for the project Generic webhook.
+#                Check Admin > Integrations > Generic API incoming webhook.
+
 echo "---> rtd-trigger-build.sh"
 
 # Ensure we fail the job if any steps fail.
 # DO NOT set -u
 set -xe -o pipefail
 
-if [ "$GERRIT_BRANCH" == "master" ]; then
-    RTD_BUILD_VERSION=latest
-else
-    RTD_BUILD_VERSION="${GERRIT_BRANCH/\//-}"
-fi
-
-curl -X POST --data "version_slug=$RTD_BUILD_VERSION" "https://readthedocs.org/build/$RTD_PROJECT"
+curl -X POST -d "branches=${GERRIT_BRANCH}" -d "token=$RTD_TOKEN" "$RTD_BUILD_URL"
