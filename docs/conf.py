406885215c3f4567b1c0773e37e5fa365db2c2fb
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-04-18 17:23:32 -0400

    Release global-jjb v0.18.0
    Change-Id: I0538f3c15e6df43369ef5e7756a76236d52d0a9e
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ author = 'Linux Foundation Releng'
 # built documents.
 #
 # The short X.Y version.
-version = '0.18.0-dev'
+version = '0.18.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
