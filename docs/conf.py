ccfdb70a9cd0326f4c8dc76a6c5f4abd01fda7d3
Anil Belur <abelur@linuxfoundation.org>
2018-05-08 22:20:04 +1000

    Release global-jjb v0.19.1
    Change-Id: Ie69a77b11ff92a40157a9fa31d70f4a9bca7aa9d
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ author = 'Linux Foundation Releng'
 # built documents.
 #
 # The short X.Y version.
-version = '0.19.0'
+version = '0.19.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
