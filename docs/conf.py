5a2ef9c64caf06ff737dfa040b61c4909c97e996
Anil Belur <abelur@linuxfoundation.org>
2018-05-09 07:59:40 +1000

    Release global-jjb v0.19.2
    Create a new tag with v0.19.2 which points to an object that changes
    the version number.
    
    Change-Id: I07282a819a14ce2edf37d71d2a0030dbcf2161d1
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ author = 'Linux Foundation Releng'
 # built documents.
 #
 # The short X.Y version.
-version = '0.19.1'
+version = '0.19.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
