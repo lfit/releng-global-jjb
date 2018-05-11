ade1838fb166e77a6faebf5ad83dd98db44c1ae9
Jeremy Phelps <jphelps@linuxfoundation.org>
2018-05-11 09:23:21 -0500

    docker-login to respect GLOBAL_SETTINGS_FILE
    This is a potentially breaking change, read
    the docs.
    The assumption here is that a given project
    will only have 1 user for dockerhub.
    A settings entry will only be required in
    one spot then; GLOBAL_SETTINGS_FILE
    
    Change-Id: I4197026c0315af9ad63c11be81c92c783ac992bc
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

--- shell/docker-login.sh
+++ shell/docker-login.sh
@@ -13,7 +13,7 @@
 
 # $DOCKER_REGISTRY : Optional
 #                    Jenkins global variable should be defined
-#                    If set, then this is the base IP or FQDN that will be used
+#                    If set, then this is the FQDN that will be used
 #                    for logging into the custom docker registry
 #                    ex: nexus3.example.com
 #
@@ -31,6 +31,9 @@
 #                      to auth to docker.io
 #
 # $SETTINGS_FILE   : Job level variable with maven settings file location
+#
+# $GLOBAL_SETTINGS_FILE : Job level variable with global settings file location.
+#                    You will define a setting for docker.io login here.
 #
 # $DOCKERHUB_EMAIL : Optional
 #                    Jenkins global variable that defines the email address that
@@ -54,10 +57,18 @@ version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1";
 set_creds() {
     set +x  # Ensure that no other scripts add `set -x` and print passwords
     echo "$1"
+
+    if [ "${1}" == "docker.io" ]
+    then
+      ENTRY_FILE=$GLOBAL_SETTINGS_FILE
+    else
+      ENTRY_FILE=$SETTINGS_FILE
+    fi
+
     CREDENTIAL=$(xmlstarlet sel -N "x=http://maven.apache.org/SETTINGS/1.0.0" \
         -t -m "/x:settings/x:servers/x:server[starts-with(x:id, '${1}')]" \
         -v x:username -o ":" -v x:password \
-        "$SETTINGS_FILE")
+        "${ENTRY_FILE}")
 
     USER=$(echo "$CREDENTIAL" | cut -f1 -d:)
     PASS=$(echo "$CREDENTIAL" | cut -f2 -d:)
