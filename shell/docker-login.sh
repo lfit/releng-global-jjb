b032f7dbea312dcdad7245666a97f47a25c2a5b4
Jeremy Phelps <jphelps@linuxfoundation.org>
2018-05-11 09:23:21 -0500

    docker-login to respect GLOBAL_SETTINGS_FILE
    This is a potentially breaking change, read the docs. The assumption
    here is that a given project will only have 1 user for dockerhub. A
    settings entry will only be required in one spot then;
    GLOBAL_SETTINGS_FILE
    
    Change-Id: I4197026c0315af9ad63c11be81c92c783ac992bc
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

--- shell/docker-login.sh
+++ shell/docker-login.sh
@@ -12,31 +12,31 @@
 # Log into a custom hosted docker registry and / or docker.io
 
 # $DOCKER_REGISTRY : Optional
-#                    Jenkins global variable should be defined
-#                    If set, then this is the base IP or FQDN that will be used
-#                    for logging into the custom docker registry
-#                    ex: nexus3.example.com
+#                    Jenkins global variable should be defined. If set, then
+#                    this is the FQDN that will be used for logging into the
+#                    custom docker registry ex: nexus3.example.com
 #
-# $REGISTRY_PORTS  : Required if DOCKER_REGISTRY is set
-#                    Jenkins global variable should be defined (space separated)
-#                    Listing of all the registry ports to login to
-#                    ex: 10001 10002 10003 10004
+# $REGISTRY_PORTS  : Required if DOCKER_REGISTRY is set Jenkins global variable
+#                    should be defined (space separated). Listing of all the
+#                    registry ports to login to ex: 10001 10002 10003 10004
 #
 # $DOCKERHUB_REGISTRY: Optional
-#                      Set global Jenkins variable to `docker.io`
-#                      Additionally you will need to add as an entry
-#                      to your projects mvn-settings file with username
-#                      and password creds.  If you are using docker version
-#                      < 17.06.0 you will need to set DOCKERHUB_EMAIL
-#                      to auth to docker.io
+#                      Set global Jenkins variable to `docker.io`. Additionally
+#                      you will need to add as an entry to your projects mvn
+#                     -settings file with username and password creds.  If you
+#                      are using docker version < 17.06.0 you will need to set
+#                      DOCKERHUB_EMAIL to auth to docker.io
 #
-# $SETTINGS_FILE   : Job level variable with maven settings file location
+# $DESIRED_SETTINGS_FILE: Optional
+#                         Job level variable with settings file location. The
+#                         defualt is the project maven settings file,
+#                         $SETTINGS_FILE.  You can opt to set this to
+#                         $GLOBAL_SETTINGS_FILE
 #
 # $DOCKERHUB_EMAIL : Optional
 #                    Jenkins global variable that defines the email address that
-#                    should be used for logging into DockerHub
-#                    Note that this will not be used if you are using
-#                    docker version >=17.06.0.
+#                    should be used for logging into DockerHub Note that this
+#                    will not be used if you are using docker version >=17.06.0.
 
 #So here are the cases in docker login
 #1) User logging into nexus no email required regardless of docker version
@@ -54,10 +54,11 @@ version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1";
 set_creds() {
     set +x  # Ensure that no other scripts add `set -x` and print passwords
     echo "$1"
+
     CREDENTIAL=$(xmlstarlet sel -N "x=http://maven.apache.org/SETTINGS/1.0.0" \
         -t -m "/x:settings/x:servers/x:server[starts-with(x:id, '${1}')]" \
         -v x:username -o ":" -v x:password \
-        "$SETTINGS_FILE")
+        "${DESIRED_SETTINGS_FILE}")
 
     USER=$(echo "$CREDENTIAL" | cut -f1 -d:)
     PASS=$(echo "$CREDENTIAL" | cut -f2 -d:)
