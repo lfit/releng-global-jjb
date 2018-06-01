56cda0367c13c16b2334c080695489c7cd44b3f7
Jessica Wagantall <jwagantall@linuxfoundation.org>
2018-05-31 20:02:38 -0700

    Add INFO.yaml verify jobs
    Add '{project-name}-info-yaml-jobs' and
    '{project-name}-github-info-yaml-jobs' to validate that
    INFO.yaml file changes are kept isolated from other file
    changes.
    
    The addition of or changes to an INFO.yaml should be kept
    on its own gerrit change and should not be combined with other
    changes.
    
    Issue: RELENG-973
    Change-Id: Id7ddb55c34a741d92b1e126f1237fb9753a4aa75
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>
    Co-Authored-By: Trevor Bramwell <tbramwell@linuxfoundation.org>

---
+++ shell/git-validate-info-yaml.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# This script will make sure that the INFO.yaml file changes are kept
+# isolated from other file changes.
+# INFO.yaml file creation or moddifications should be kept in its own separate
+# Gerrit.
+
+# This script will fail if other file changes are also included in the same
+# patch.
+
+# Ensure we fail the job if any steps fail.
+set -e -o pipefail
+set +u
+
+MODIFIED_FILES=$(git diff --name-only HEAD~1)
+
+if [ "$MODIFIED_FILES" != "INFO.yaml" ]; then
+    echo 'ERROR: Do not combine INFO.yaml file changes with other files. Please isolate INFO.yaml changes.'
+    exit 1
+fi
---
+++ shell/info-file-validate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo '--> info-file-validate.sh'
+set -eu -o pipefail
+set -x  # Enable trace
+
+# Activate virtualenv, supressing shellcheck warning
+# shellcheck source=/dev/null
+. $WORKSPACE/venv/bin/activate
+source "/tmp/v/info/bin/activate"
+pip install PyYAML jsonschema rfc3987
+
+MODIFIED_FILES=$(git diff HEAD^1 --name-only -- "**.y*ml")
+
+# Cloning global-jjb to get access to needed scripts
+git clone git@github.com:lfit/releng-global-jjb.git
+cd releng-global-jjb
+
+for yaml_file in $MODIFIED_FILES; do
+    python yaml-verify-schema.py \
+    -s shell/info-schema \
+    -y $yaml_file
+done
+cd ..
+rm -rf releng-global-jjb
