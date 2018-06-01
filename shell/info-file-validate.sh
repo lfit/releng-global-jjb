d8ac5c1997126b6ba4993ea8d9cb58ecefa555c2
Jessica Wagantall <jwagantall@linuxfoundation.org>
2018-05-31 20:02:38 -0700

    Add INFO.yaml verify jobs
    Add '{project-name}-info-yaml-jobs' and
    '{project-name}-github-info-yaml-jobs' to validate that
    INFO.yaml file changes are kept isolated from other file
    changes.
    
    The addition of or changes to an INFO.yaml should be kept
    on its own gerrit change and should not be combined with other
    changes.
    
    Issue: RELENG-973
    Change-Id: Id7ddb55c34a741d92b1e126f1237fb9753a4aa75
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>
    Co-Authored-By: Trevor Bramwell <tbramwell@linuxfoundation.org>

---
+++ shell/info-file-validate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo '--> info-file-validate.sh'
+set -e -o pipefail
+set -x  # Enable trace
+
+virtualenv --quiet "/tmp/v/info"
+# shellcheck source=/tmp/v/info/bin/activate disable=SC1091
+source "/tmp/v/info/bin/activate"
+pip install PyYAML jsonschema rfc3987
+
+MODIFIED_FILES='INFO.yaml'
+
+# Cloning global-jjb to get access to needed scripts
+git clone https://gerrit.linuxfoundation.org/infra/releng/global-jjb
+
+for yaml_file in $MODIFIED_FILES; do
+    python global-jjb/yaml-verify-schema.py \
+    -s global-jjb/info-schema \
+    -y $yaml_file
+done
+rm -rf global-jjb
