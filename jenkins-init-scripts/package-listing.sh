a079f1b34f9f3ae69dd5f996682514db667fe82d
Andrew Grimberg <agrimberg@linuxfoundation.org>
2018-06-07 14:35:02 -0700

    Add package lists to job logs
    A listing of packages that are on the system is useful for debugging
    changes that happen during a job run
    
    Issue: RELENG-798
    Change-Id: I88bc2eca175bbef1b7581b26199aff9a13f1910d
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

---
+++ jenkins-init-scripts/package-listing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# Share script with JJB jobs so we only have to maintain it in one place
+JJB_SHELL_DIR="$(dirname $0)/../shell"
+
+# Make sure the script is executable and then run it
+chmod +x "${JJB_SHELL_DIR}/package-listing.sh"
+"${JJB_SHELL_DIR}/package-listing.sh"
