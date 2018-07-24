cecf48f14438cef791a58d704a9aa169f47d3552
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-07-23 20:54:58 -0400

    Add cmake-verify job template
    CMake verify job template for building CMake based projects.
    
    Issue: RELENG-1074
    Change-Id: Iccd3bab1d9f17071583a03e84b35bcd729161195
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/cmake-build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> cmake-build.sh"
+
+build_dir="${BUILD_DIR:-$WORKSPACE/target}"
+cmake_opts="${CMAKE_OPTS:-}"
+make_opts="${MAKE_OPTS:-}"
+project="${PROJECT//\//\-}"
+
+################
+# Script start #
+################
+
+set -eux -o pipefail
+
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR" || exit
+cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" $cmake_opts ..
+make $make_opts
+make install
+
+mkdir -p "$WORKSPACE/dist"
+tar -cJvf "$WORKSPACE/dist/$project.tar.xz" -C "$INSTALL_PREFIX" .
+
+sudo tar -xvf "$WORKSPACE/dist/$project.tar.xz" -C "/usr/local"
+sudo ldconfig
