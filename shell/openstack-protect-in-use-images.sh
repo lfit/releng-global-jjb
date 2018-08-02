5ac03c9b791c9759f3d4e125f4eeda495ab5da18
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-08-02 18:23:02 -0400

    Move openstack-cron job to global-jjb
    Move OpenDaylight's builder-openstack-cron job to global-jjb. This is
    an initial patch that only migrates the "image-protection" script.
    
    image-protection is used to parse jenkins-config/ and jjb/ directories
    for any mention of images starting with "ZZCI" and flagging them as
    protected. The assumption here is that ZZCI images are CI managed
    and if they are referred to anywher by anything then they are
    effectively considered in-use and should not be deletable so flag
    them as "protected: true" in OpenStack.
    
    Change-Id: Ib706663f0c08bd7a9160b0a6ce69dcd3b802f8f5
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/openstack-protect-in-use-images.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017, 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+# Checks the image "protected" value and set "True" marker
+#
+# The script searches the ciman repo for the images presently used and ensures
+# the protection setting is set for those images to prevent the image from
+# getting purged by the image cleanup script.
+# This script assumes images prefixed with the string "ZZCI - " are ci-managed
+# images.
+echo "---> Protect in-use images"
+
+os_cloud="${OS_CLOUD:-vex}"
+
+set -eu -o pipefail
+
+declare -a yaml_images
+readarray -t yaml_images <<< "$(grep -r 'ZZCI - ' --include \*.yaml \
+    | awk -F": " -e '{print $3}' | sed "s:'::;s:'$::;/^$/d" | sort -u)"
+declare -a cfg_images
+readarray -t cfg_images <<< "$(grep -r IMAGE_NAME --include \*.cfg \
+    | awk -F'=' -e '{print $2}' | sort -u)"
+combined=("${yaml_images[@]}" "${cfg_images[@]}")
+declare -a images
+readarray -t images <<< "$(printf '%s\n' "${combined[@]}" | sort -u)"
+
+for image in "${images[@]}"; do
+    os_image_protected=$(openstack --os-cloud "$os_cloud" \
+        image show "$image" -f value -c protected)
+    echo "Protected setting for $image: $os_image_protected"
+
+    if [[ $os_image_protected != True ]]; then
+        echo "    Image NOT set as protected, changing the protected value."
+        openstack --os-cloud "$os_cloud" image set --protected "$image"
+    fi
+done
