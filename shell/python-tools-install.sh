8a35e1d85e715da711e240ff08c2f66e24ed52e7
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-08-07 20:27:59 -0400

    Add pre-build step to prep vm before build
    This new build step will run before any jobs run to prep the
    builder.
    
    Change-Id: I55947020e2f0cdbb61f9d4e7e88927d9bc304d17
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/python-tools-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+set -eux -o pipefail
+
+REQUIREMENTS_FILE=$(mktemp /tmp/requirements-XXXX.txt)
+
+cat << EOF > "$REQUIREMENTS_FILE"
+lftools~=0.15.0
+python-heatclient~=1.16.1
+python-openstackclient~=3.16.0
+EOF
+
+pip install --user --quiet --upgrade pip==18.0 setuptools==40.0.0
+pip install --user --quiet --upgrade -r "$REQUIREMENTS_FILE"
