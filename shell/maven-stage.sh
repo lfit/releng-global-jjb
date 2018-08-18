2f43e2ee13855a63f793d2991f1a3dbf064c1cec
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-08-18 12:26:15 -0400

    Add option to stage to Maven Central
    Provide an option to also stage to OSSRH in preparation to release
    to Maven Central.
    
    In addition to printing the staging repo-id also print the URL to
    the staging repo for convenience in stage-repo.txt.
    
    Issue: RELENG-1398
    Issue: https://jira.opendaylight.org/browse/RELENG-28
    Change-Id: I0628cb5c7185e2143e448ceb4fa4752384581a5e
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- shell/maven-stage.sh
+++ shell/maven-stage.sh
@@ -24,7 +24,7 @@ staging_repo=$(sed -n -e 's/Staging repository \(.*\) created\./\1/p' "$TMP_FILE
 
 # Store repo info to a file in archives
 mkdir -p "$WORKSPACE/archives"
-echo "$staging_repo" > "$WORKSPACE/archives/staging-repo.txt"
+echo "$staging_repo ${NEXUS_URL}/content/repositories/$staging_repo" | tee -a "$WORKSPACE/archives/staging-repo.txt"
 
 # Cleanup
 rm -f "$TMP_FILE"
