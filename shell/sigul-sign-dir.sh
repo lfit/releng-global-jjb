7ee13eac4b445ff90b893c2052a3075a0d774a4f
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-08-17 22:15:07 -0400

    Add sigul signing to maven-stage job
    Refactor the lf-sigul* macros into a new one called lf-sigul-sign-dir
    to be used as a one shot macro to sign a directory. Macro initializes
    and cleans up after itself so no need to call extra builder steps.
    
    Issue: RELENG-1071
    Change-Id: I0ff965c2ef24fa7a44a114445d8a97c3176e575a
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/sigul-sign-dir.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> sigul-sign-dir.sh"
+
+# Ensure we fail the job if any steps fail.
+set -e -o pipefail
+
+lftools_activate
+lftools sign sigul "${SIGN_DIR}"
