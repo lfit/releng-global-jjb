800d9d69a51fbf7d5674c46ad800ca8a1df23e07
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-09-13 15:31:30 -0400

    Use --user for tox install instead of virtualenv
    Allows us to remove the workaround of installing lftools in
    a venv before as the virtualenvs should no longer be conflicting.
    
    Change-Id: Ic466cd74a568e0de2fbb0f3fd75205cd04c1e3f9
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- shell/tox-install.sh
+++ shell/tox-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 # SPDX-License-Identifier: EPL-1.0
 ##############################################################################
 # Copyright (c) 2017 The Linux Foundation and others.
@@ -14,11 +14,7 @@ echo "---> tox-install.sh"
 # DO NOT set -u as virtualenv's activate script has unbound variables
 set -e -o pipefail
 
-virtualenv --quiet -p "$PYTHON_VERSION" "/tmp/v/tox"
-# shellcheck source=/tmp/v/tox/bin/activate disable=SC1091
-source "/tmp/v/tox/bin/activate"
-pip install --quiet --upgrade "pip==9.0.3" setuptools
-pip install --quiet --upgrade argparse detox "tox<3.0.0" tox-pyenv
+python -m pip install --quiet --upgrade argparse detox "tox<3.0.0" tox-pyenv
 
 echo "----> pip freeze"
 pip freeze
