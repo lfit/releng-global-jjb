9e27a8a8dccf180301863a3121dd7b730723a321
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-09-13 15:31:30 -0400

    Use --user for tox install instead of virtualenv
    Allows us to remove the workaround of installing lftools in
    a venv before as the virtualenvs should no longer be conflicting.
    
    Change-Id: Ic466cd74a568e0de2fbb0f3fd75205cd04c1e3f9
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- shell/python-tools-install.sh
+++ shell/python-tools-install.sh
@@ -19,6 +19,7 @@ python-heatclient~=1.16.1
 python-openstackclient~=3.16.0
 EOF
 
-pip install --user --quiet --upgrade pip==18.0 setuptools==40.0.0
-pip install --user --quiet --upgrade -r "$REQUIREMENTS_FILE"
+# Use `python -m pip` to ensure we are using the latest version of pip
+python -m pip install --user --quiet --upgrade pip==18.0 setuptools==40.0.0
+python -m pip install --user --quiet --upgrade -r "$REQUIREMENTS_FILE"
 pip freeze
