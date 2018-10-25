48f353e9aa435322ea031d7aacd818c67f4af9b4
Eric Ball <eball@linuxfoundation.org>
2018-10-25 14:54:32 -0700

    Verify jenkins-config files have matching images
    This verify job will check that for each config file in
    jenkins-config/clouds/**, there is an image on the cloud provider
    that matches the IMAGE_NAME field.
    
    Issue-ID: RELENG-1133
    Change-Id: I9bf79657af27e8c7ff7b63ced2bd8a0dd544e6d6
    Signed-off-by: Eric Ball <eball@linuxfoundation.org>

---
+++ shell/jenkins-verify-images.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> jenkins-verify-images.sh"
+# Verifies that openstack contains an image for each config file defined in the
+# jenkins-config/clouds/openstack/cattle directory.
+
+# Ensure we fail the job if any steps fail.
+# DO NOT set -u as virtualenv's activate script has unbound variables
+
+error=false
+
+for file in jenkins-config/clouds/openstack/*/*; do
+  # Set the $IMAGE_NAME variable to the the file's IMAGE_NAME value
+  export "$(grep IMAGE_NAME $file)"
+  # The image should be listed as active
+  openstack image list --property name="$IMAGE_NAME" | grep "active"
+  if [ $? -ne 0 ]; then
+    echo "ERROR: No matching image found for $IMAGE_NAME"
+    error=true
+  fi
+done
+
+if [ "$error" = true ]; then
+  exit 1
+fi
