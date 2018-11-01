237b45f45d16b5c8eab677cd1ed7690b4bf9acfc
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-10-31 23:39:39 -0400

    Add job to automatically announce lf releases
    This job triggers when a Gerrit tag is pushed and sends an email to
    lf-releng@lists.linuxfoundation.org to notifiy the release.
    
    Change-Id: I5255ac6f3afbcadfc11e9640483613bd2e84250a
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ shell/release-announce.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+PROJECT="${GERRIT_PROJECT#releng/}"  # For releng projects strip the prefix
+TAG_NAME="${GERRIT_REFNAME#refs/tags/}"
+
+set -eux -o pipefail
+
+mail_opts=()
+mail_opts+=("-r" "LF Releng <lf-releng@lists.linuxfoundation.org>")
+mail_opts+=("-s" "$PROJECT $TAG_NAME released")
+mail_opts+=("lf-releng mailing list <lf-releng@lists.linuxfoundation.org>")
+
+mail_body="Hi Everyone,
+
+$PROJECT $TAG_NAME is released. Thanks to everyone who contributed
+to this release. Release notes are available online at:
+
+https://docs.releng.linuxfoundation.org/projects/$PROJECT/en/latest/release-notes.html#${TAG_NAME}
+
+Cheers,
+LF Releng
+"
+eval echo \""$mail_body"\" | mail "${mail_opts[@]}"
