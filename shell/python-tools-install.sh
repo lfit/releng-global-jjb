9cbf5ed0eb56a572627dac27f0ef157f8af0e674
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-12-03 14:48:14 +0800

    Bump lftools version to v0.19.0
    Change-Id: Ib28022b61efe42bbad77418d4289ddf3b9afd75f
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- shell/python-tools-install.sh
+++ shell/python-tools-install.sh
@@ -14,7 +14,7 @@ set -eux -o pipefail
 REQUIREMENTS_FILE=$(mktemp /tmp/requirements-XXXX.txt)
 
 cat << EOF > "$REQUIREMENTS_FILE"
-lftools[openstack]~=0.18.0
+lftools[openstack]~=0.19.0
 python-heatclient~=1.16.1
 python-openstackclient~=3.16.0
 EOF
