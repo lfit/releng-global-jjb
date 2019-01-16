1d995a14adb7ae1a8c63743f52c440a4a533d377
Aric Gardner <agardner@linuxfoundation.org>
2019-01-16 16:45:30 -0500

    Create branches and tags from release files
    Implementing opnfv's create branches and tags from release files in
    global jjb.
    
    project will have a project_name.yaml file in
    ci-management/releases/$stream/$project_name.yaml
    
    in that file they can set a ref to branch or tag at.
    
    example: https://gerrit.opnfv.org/gerrit/#/c/65859/
    
    As a next step I am going to automate releasing artifacts by this
    same method.
    
    an additional script 'releng-release-repos.py'
    provides an quick interface for pulling out information
    from release files along with the script
    
    This is my first commit, so I've probably missed some things.
    
    Change-Id: I02dc935a84425d194cdd8eaabe5b43c5920ce319
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

---
+++ shell/releng-release-tagging.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -e -o pipefail
+
+GIT_URL=${GIT_URL}
+RELEASE_FILES=$(git diff HEAD^1 --name-only -- "releases/")
+
+#this wont work if there are multiple files changed.
+STREAM=$(git diff HEAD^1 --name-only -- "releases/" | awk -F"/" '{print $2}')
+
+echo "--> Verifying $RELEASE_FILES."
+for release_file in $RELEASE_FILES; do
+
+    # Verify tag for each repo exist and are attached to commits on stable-branch
+    while read -r repo tag ref
+    do
+      echo "--> Cloning $repo"
+      if [ ! -d $repo ]; then
+          git clone $GIT_URL/$repo.git $repo
+      fi
+      pushd $repo &> /dev/null
+
+      echo "--> Checking for tag: $tag"
+      if ! (git tag -l | grep $tag &> /dev/null); then
+          echo "$tag does not exist"
+          TAG_EXISTS=false
+      else
+          git cat-file commit $tag
+          TAG_EXISTS=true
+      fi
+
+      echo "--> Checking if $ref is on stable/$STREAM"
+      if ! (git branch -a --contains $ref | grep "stable/$STREAM"); then
+          echo "--> ERROR: $ref for $repo is not on stable/$STREAM!"
+          # If the tag exists but is on the wrong ref, there's nothing
+          # we can do. But if the tag neither exists nor is on the
+          # correct branch we need to fail the verification.
+          if [ $TAG_EXISTS = false ]; then
+              exit 1
+          fi
+      else
+          if [[ $TAG_EXISTS = false && "$JOB_NAME" =~ "merge" ]]; then
+              # If the tag doesn't exist and we're in a merge job,
+              # everything has been verified up to this point and we
+              # are ready to create the tag.
+              git config --global user.name "jenkins-ci"
+              git config --global user.email "jenkins-opnfv-ci@opnfv.org"
+              echo "--> Creating $tag tag for $repo at $ref"
+              git tag -am "$tag" $tag $ref
+              echo "--> Pushing tag"
+              git push origin $tag
+          else
+              # For non-merge jobs just output the ref info.
+              git show -s --format="%h %s %d" $ref
+          fi
+      fi
+
+      popd &> /dev/null
+      echo "--> Done verifing $repo"
+    done < <(python global-jjb/shell/releng-release-repos.py -f "$release_file")
+
+done
