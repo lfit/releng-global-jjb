b4400baf3031efa657fc39c3e39f9061bc65156e
Aric Gardner <agardner@linuxfoundation.org>
2019-01-16 16:45:30 -0500

    Automate ODL tagging and artifact release
    This will be adapted to work for ONAP/Other projects
    once we are okay with this.
    
    projects that want self-serve releases will create a
    tagname.yaml file in the releases/ directory of their git repo.
    
    example:
    $ cat releases/4.0.0.yaml
    ---
    artifact_type: 'maven'
    version: '4.0.0'
    project: 'odlparent'
    log_dir: 'odlparent-maven-release-master/11/'
    maven_central_url: 'oss.sonatype.org'
    
    We might be able to remove some of these fields,
    but this is good for a first review.
    
    Comments on the current limitations
    inline in shell/release-job.sh
    
    Issue: RELENG-1657
    
    Change-Id: I02dc935a84425d194cdd8eaabe5b43c5920ce319
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

---
+++ shell/release-job.sh
@@ -0,0 +1,147 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+#set -e -o pipefail
+set +x
+
+
+echo "#######################################"
+
+if [ -d "/opt/pyenv" ]; then
+    echo "---> Setting up pyenv"
+    export PYENV_ROOT="/opt/pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+fi
+PYTHONPATH=$(pwd)
+export PYTHONPATH
+pyenv local 3.6.4
+export PYENV_VERSION="3.6.4"
+
+pip3 install --user niet
+pip3 install --user lftools
+pip3 install --user lftools[nexus]
+
+echo "$PATH"
+
+#provided by jenkins
+NEXUS_URL=${NEXUS_URL}
+ODLNEXUSPROXY=${ODLNEXUSPROXY}
+JENKINS_HOSTNAME=${JENKINS_HOSTNAME}
+SILO=${SILO}
+LOGS_SERVER="${LOGS_SERVER:-None}"
+#
+if [ "${LOGS_SERVER}" == 'None' ]; then
+  echo "FAILED: log server not found"
+  exit 1
+else
+  NEXUS_URL="${ODLNEXUSPROXY:-$NEXUS_URL}"
+  NEXUS_PATH="${SILO}/${JENKINS_HOSTNAME}/"
+fi
+
+RELEASE_FILES=$(git diff HEAD^1 --name-only -- "releases/")
+echo "RELEASE FILES ARE AS FOLLOWS: $RELEASE_FILES"
+#overwrite release files for testing
+RELEASE_FILES="releases/1.0.0.yaml"
+
+#need pip install jsonschema for this.
+echo "--> Verifying $RELEASE_FILES Schema."
+for release_file in $RELEASE_FILES; do
+  #not schema file not yet written
+  echo "DUMMY CODE:"
+  echo "lftools schema verify [OPTIONS] $release_file $SCHEMAFILE"
+done
+
+for release_file in $RELEASE_FILES; do
+  echo "This is the release file: $release_file"
+
+  VERSION="$(niet ".version" "$release_file")"
+  PROJECT="$(niet ".project" "$release_file")"
+  LOG_DIR="$(niet ".log_dir" "$release_file")"
+
+# need some sort of if here incase its unset
+# MAVEN_CENTRAL_URL="$(niet ".maven_central_url" "$release_file")"
+
+  #Hard code for testing..
+  #NEXUS_PATH="${SILO}/${JENKINS_HOSTNAME}/"
+  #NEXUS_PATH=sandbox/vex-yul-odl-jenkins-2/
+  #WE NEED NEXUS_PATH=releng/vex-yul-odl-jenkins-1/
+  NEXUS_PATH=releng/vex-yul-odl-jenkins-1/
+  LOGS_URL="$LOGS_SERVER"/"$NEXUS_PATH""$LOG_DIR"
+  PATCH_DIR=/tmp/patches
+
+  mkdir -p "$PATCH_DIR"
+  cd "$PATCH_DIR" || exit
+
+  rm -f "${PROJECT}".bundle taglist.log{,.gz} staging-repo.txt.gz
+
+
+  #Do we need a check for cases where things are not gzipped?
+  #mine dont have .gz 
+# wget https://logs.opendaylight.org/releng/vex-yul-odl-jenkins-1/zzz-test-release-maven-stage-master/17/staging-repo.txt
+  #hmm we need to check for cases of a missing / on log_dir
+  wget --quiet "${LOGS_URL}"staging-repo.txt
+  #STAGING_REPO="$(zcat staging-repo.txt)"
+  STAGING_REPO="$(cat staging-repo.txt)"
+
+#INFO
+echo "INFO:"
+echo "RELEASE_FILES: $RELEASE_FILES"
+echo "LOGS_SERVER: $LOGS_SERVER"
+echo "NEXUS_URL: $NEXUS_URL"
+echo "NEXUS_PATH: $NEXUS_PATH"
+echo "ODLNEXUSPROXY: $ODLNEXUSPROXY"
+echo "JENKINS_HOSTNAME: $JENKINS_HOSTNAME"
+echo "SILO: $SILO"
+echo "PROJECT: $PROJECT"
+echo "STAGING_REPO: $STAGING_REPO"
+
+#  if [[ "$JOB_NAME" =~ "merge" ]]; then
+#    echo "Running merge"
+#    if ! [ -z "$NEXUS_URL" ]; then
+
+#     NEEDS: 
+ #    - config-file-provider:
+ #         files:
+ #           - file-id: lftoolsini
+ #             target: '$HOME/.config/lftools/lftools.ini'
+ echo "lftools nexus release --server "$NEXUS_URL" "$STAGING_REPO""
+#    fi
+
+#    if ! [ -z "$MAVEN_CENTRAL_URL" ]; then
+#      lftools nexus release --server "$MAVEN_CENTRAL_URL" "$STAGING_REPO"
+#    fi
+
+
+#16:30:42 /tmp/jenkins4256085658751084108.sh: line 104: lftools: command not found
+#16:30:42 gzip: taglist.log.gz: No such file or directory
+
+    #wget --quiet  "${LOGS_URL}"/patches/{"${PROJECT}".bundle,taglist.log.gz}
+    wget --quiet  "${LOGS_URL}"/patches/{"${PROJECT}".bundle,taglist.log}
+    #gunzip taglist.log.gz
+    cd -
+    git checkout "$(awk '{print $NF}' "$PATCH_DIR/taglist.log")"
+    git fetch "$PATCH_DIR/$PROJECT.bundle"
+    git merge --ff-only FETCH_HEAD
+
+#zxiiro [2:53 PM]
+#@aric jenkins doesn't currently sign. it's not something we sovled yet
+#@aric we'll need to figure out how to make sigul do it someday though
+
+echo "tag -asm "$PROJECT $VERSION" "v$VERSION""
+  echo "NOT DOING THIS PART  git push origin "v$VERSION""
+
+  #else
+  #  echo "Verification Complete"
+#  fi
+
+#this doesnt work if its  merge job
+#  cd -
+
+done
