28ea3ab0bd1aca3d00cf5428bd805481e137f0c8
Eric Ball <eball@linuxfoundation.org>
2019-01-31 15:18:21 -0800

    Verify build node HARDWARE_ID
    This script already checks each file's IMAGE_NAME for validity; this
    adds essentially the same validation to check the HARDWARD_ID.
    Also add the job to the ci-jobs groups, and set submodule-recursive
    value for github-jenkins-cfg-verify.
    
    Issue: RELENG-1418
    Change-Id: I0266a0fcc276b2960c9c1749a89bc70e8115d152
    Signed-off-by: Eric Ball <eball@linuxfoundation.org>

--- shell/jenkins-verify-images.sh
+++ shell/jenkins-verify-images.sh
@@ -12,19 +12,29 @@ echo "---> jenkins-verify-images.sh"
 # Verifies that openstack contains an image for each config file defined in the
 # jenkins-config/clouds/openstack directory.
 
+set -eux -o pipefail
+
 error=false
 
 for file in jenkins-config/clouds/openstack/*/*; do
   # Set the $IMAGE_NAME variable to the the file's IMAGE_NAME value
-  export "$(grep IMAGE_NAME $file)"
+  export "$(grep ^IMAGE_NAME= $file)"
   # The image should be listed as active
-  openstack image list --property name="$IMAGE_NAME" | grep "active"
-  if [ $? -ne 0 ]; then
+
+  if ! openstack image list --property name="$IMAGE_NAME" | grep "active"; then
     echo "ERROR: No matching image found for $IMAGE_NAME"
+    error=true
+  fi
+  # Set the $HARDWARE_ID variable to the the file's HARDWARE_ID value
+  export "$(grep ^HARDWARE_ID= $file)"
+  # The flavor should be listed. Spaces in grep string ensure complete match.
+
+  if ! openstack flavor list | grep " $HARDWARE_ID "; then
+    echo "ERROR: No matching flavor found for $HARDWARE_ID"
     error=true
   fi
 done
 
-if [ "$error" = true ]; then
+if $error; then
   exit 1
 fi
