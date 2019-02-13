98f2daa9c0dd4422da84088e763c4570afc8b0c7
Jessica Wagantall <jwagantall@linuxfoundation.org>
2019-02-13 10:13:19 -0800

    Add Docker Verify and Merge jobs
    Add {project-name}-{stream}-verify-docker and
    {project-name}-{stream}-merge-docker jobs.
    
    Issue: RELENG-908
    Change-Id: I009fea61fd4c13daebac0d14f5dc00e1a90011e7
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

---
+++ shell/docker-push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# Docker image push script
+
+# Ensure we fail the job if any steps fail
+set -ue -o pipefail
+
+docker push "$CONTAINER_PUSH_REGISTRY/$DOCKER_IMAGE"
