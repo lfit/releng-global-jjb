8f00e059ea64956604ea2d23ebe46e4fda788cdf
Jessica Wagantall <jwagantall@linuxfoundation.org>
2019-02-13 10:13:19 -0800

    Add Docker Verify and Merge jobs
    Add {project-name}-docker-verify-{stream} and
    {project-name}-docker-merge-{stream} jobs.
    
    Issue: RELENG-908
    Change-Id: I009fea61fd4c13daebac0d14f5dc00e1a90011e7
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

---
+++ shell/docker-get-yaml-tag.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> docker-get-yaml-tag.sh"
+# Gets the container tag from a yaml file.
+
+set -eu -o pipefail
+
+cd "$DOCKER_ROOT"
+container_tag_file=container-tag.yaml
+
+if [ -f "$container_tag_file" ]
+then
+    image_name=$(yq -r .tag "$container_tag_file")
+else
+    echo "$container_tag_file file not found. Make sure this file exists."
+    exit 1
+fi
+
+# Write DOCKER_IMAGE information to a file so it can be injected into the
+# environment for following steps
+echo "DOCKER_IMAGE=$image_name" >> "$WORKSPACE/env_docker_inject.txt"
