91aa99d80a5013e1565986de0a05cdaff4f8aea6
Jessica Wagantall <jwagantall@linuxfoundation.org>
2019-02-13 10:13:19 -0800

    Add Docker Verify and Merge jobs
    Add {project-name}-{stream}-verify-docker and
    {project-name}-{stream}-merge-docker jobs.
    
    Issue: RELENG-908
    Change-Id: I009fea61fd4c13daebac0d14f5dc00e1a90011e7
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

---
+++ shell/docker-build-param-version.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# Docker image build script
+
+set -eu -o pipefail
+
+cd "$DOCKER_ROOT"
+
+# DOCKERREGISTRY is purposely not using an '_' so as to not conflict with the
+# Jenkins global env var of the DOCKER_REGISTRY which the docker-login step uses
+image_name="$DOCKERREGISTRY/$DOCKER_NAME:$DOCKER_TAG"
+image_name_latest="$DOCKERREGISTRY/$DOCKER_NAME:$DOCKER_LATEST_TAG"
+
+docker build "$DOCKER_ARGS" . -t "$image_name" -t "$image_name_latest" | tee "$WORKSPACE/docker_build_log.txt"
+
+# Write DOCKER_IMAGE information to a file so it can be injected into the
+# environment for following steps
+echo "DOCKER_IMAGE=$image_name" >> "$WORKSPACE/env_inject.txt"
+echo "DOCKER_IMAGE_latest=$image_name_latest" >> "$WORKSPACE/env_inject.txt"
+
+
