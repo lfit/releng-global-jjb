36a0cd1fad208c310c7dcff1f27a9e407209d546
Jessica Wagantall <jwagantall@linuxfoundation.org>
2019-02-13 10:13:19 -0800

    Add Docker Verify and Merge jobs
    Add {project-name}-{stream}-verify-docker and
    {project-name}-{stream}-merge-docker jobs.
    
    Issue: RELENG-908
    Change-Id: I009fea61fd4c13daebac0d14f5dc00e1a90011e7
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

---
+++ shell/docker-build-git-params.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> docker-build-git-params.sh"
+# Docker image build script
+
+set -eu -o pipefail
+
+cd "$DOCKER_ROOT"
+
+image_build_tag=$(git describe)
+
+if [ -z "$image_build_tag" ]
+then
+    image_name="$CONTAINER_PUSH_REGISTRY/$DOCKER_NAME"
+else
+    image_name="$CONTAINER_PUSH_REGISTRY/$DOCKER_NAME:$image_build_tag"
+fi
+
+docker build "$DOCKER_ARGS" . -t "$image_name" | tee "$WORKSPACE/docker_build_log.txt"
+# Write DOCKER_IMAGE information to a file so it can be injected into the
+# environment for following steps
+echo "DOCKER_IMAGE=$image_name" >> "$WORKSPACE/env_docker_inject.txt"
