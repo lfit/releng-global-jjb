dfb921c387aac0692c8fb72bb1fae9874b652ae5
Tim Johnson <tijohnson@linuxfoundation.org>
2019-03-05 14:46:52 -0800

    Update Jenkins Init Scripts
    Support ability to specify swap size and to pre-allocate the '/w' volume
    for the Hyperledger Project
    
    Change-Id: I04af514bbb237f405eb1d31ec10e3f019c85a43e
    Signed-off-by: Tim Johnson <tijohnson@linuxfoundation.org>

--- jenkins-init-scripts/create-swap-file.sh
+++ jenkins-init-scripts/create-swap-file.sh
@@ -9,7 +9,24 @@
 # http://www.eclipse.org/legal/epl-v10.html
 ##############################################################################
 
-dd if=/dev/zero of=/swap count=1024 bs=1MiB
+# Get the blockCount from the 'SWAP_COUNT' environmental variable
+swapCount=${SWAP_COUNT-''}
+
+# Validate SWAP_COUNT
+# Empty:   Set blockCount 1
+# Zero:    No Swap
+# Integer: Set blockCount
+# Else:    No Swap
+case $swapCount in
+    '')      blockCount=1 ;;
+    [0-9]*)  blockCount=$swapCount ;;
+    *)       exit ;;
+esac
+[[ $blockCount == 0 ]] && exit
+
+echo "Allocating ${blockCount}GB of Swap"
+
+dd if=/dev/zero of=/swap count="${swapCount}k" bs=1MiB
 chmod 600 /swap
 mkswap /swap
 swapon /swap
