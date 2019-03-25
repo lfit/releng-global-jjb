6e425c472fab712fd18bed066cdd530b5f57c242
Aric Gardner <agardner@linuxfoundation.org>
2019-03-25 16:35:34 -0400

    Preliminary INFO.yaml check vote
    Startings of a single job that
    will check votes when any change
    is made to an INFO.yaml file
    in a projects gerrit.
    
    Change-Id: Ib386bd2ea51be113ca59610bd3281b9ad6923bce
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

---
+++ shell/check-info-votes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> check-info-votes.sh"
+
+# Ensure we fail the job if any steps fail.
+set -xe -o pipefail
+
+lftools infofile check-votes INFO.yaml
