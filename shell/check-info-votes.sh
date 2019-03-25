25e23dd8cab5b24223da654a72dc0b0f8a1cda26
Aric Gardner <agardner@linuxfoundation.org>
2019-03-25 16:35:34 -0400

    INFO.yaml check vote for global-jjb
    Global job per gerrit, that triggers on changes
    to INFO.yaml file
    
    And auto merged if a majority of committers
    and tsc members have voted.
    
    We will need to give
    Non-Interactive Users merge
    or perhaps specifically only the jenkins
    user.
    
    Job will only trigger when a user votes +2
    cludgy but it works.
    
    Example of Job configuration:
    
    - project:
        name: info-vote
        build-node: centos7-builder-2c-1g
        jobs:
          - 'info-vote-verify'
    
    Change-Id: Ib386bd2ea51be113ca59610bd3281b9ad6923bce
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

---
+++ shell/check-info-votes.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> check-info-votes.sh"
+set -xe -o pipefail
+
+ref=$(echo "$GERRIT_REFSPEC" | awk -F"/" '{ print $4 }')
+
+# For OPNFV
+if [[ $NODE_NAME =~ "lf-build" ]]; then
+  pip install --user niet
+  pip install --user lftools
+  pip install --user lftools[nexus]
+  pip install --user jsonschema
+
+  lftools infofile check-votes INFO.yaml "$GERRIT_URL" "$ref"
+
+# For All other projects.
+else
+
+  if [ -d "/opt/pyenv" ]; then
+      echo "---> Setting up pyenv"
+      export PYENV_ROOT="/opt/pyenv"
+      export PATH="$PYENV_ROOT/bin:$PATH"
+  fi
+  PYTHONPATH=$(pwd)
+  export PYTHONPATH
+  pyenv local 3.6.4
+  export PYENV_VERSION="3.6.4"
+
+  pip3 install --user niet
+  pip3 install --user lftools
+  pip3 install --user lftools[nexus]
+  pip3 install --user jsonschema
+
+  #On the fly info file from the tsc members in INFO-master.yaml
+  project="$(echo $GERRIT_URL | awk -F"." '{print $2}')"
+  curl -o /tmp/INFO-master.yaml "https://gerrit.linuxfoundation.org/infra/gitweb?p=releng/info-master.git;a=blob_plain;f=INFO-master.yaml;hb=HEAD"
+  printf -- "---\ncommitters:\n" > /tmp/INFO.yaml
+  for x in $(niet ".$project.tsc" /tmp/INFO-master.yaml); do echo "    - id: "$x >> /tmp/INFO.yaml; done
+
+  echo "Generated TSC INFO file:"
+  cat /tmp/INFO.yaml
+
+  change="$(echo "$GERRIT_CHANGE_URL" | awk -F"/" '{print $NF}')"
+  echo "Checking votes:"
+  lftools infofile check-votes INFO.yaml "$GERRIT_URL" "$ref" --tsc /tmp/INFO.yaml
+  exit_status="$?"
+
+  if [[ "$exit_status" -ne 0 ]]; then 
+    echo "Vote not yet complete"
+    exit "$exit_status"
+  else
+    echo "Vote completed submitting review"
+    ssh -p "$GERRIT_PORT" "$JENKINS_SSH_CREDENTIAL"@"$GERRIT_HOST" gerrit review "$change" --submit
+  fi
+
+fi
