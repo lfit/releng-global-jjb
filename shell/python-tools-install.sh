a31f4f1723085c461305414e1051d5adb7e64267
Tim Johnson <tijohnson@linuxfoundation.org>
2019-04-09 14:41:20 -0700

    Reduce output in log files
    Issue: RELENG-1902
    Change-Id: Ib0f99bd1eb125015467c7eaf91d26c76e92bffa0
    Signed-off-by: Tim Johnson <tijohnson@linuxfoundation.org>

--- shell/python-tools-install.sh
+++ shell/python-tools-install.sh
@@ -9,15 +9,16 @@
 # http://www.eclipse.org/legal/epl-v10.html
 ##############################################################################
 
-set -eux -o pipefail
+set -eu -o pipefail
 
-REQUIREMENTS_FILE=$(mktemp /tmp/requirements-XXXX.txt)
+requirements_file=$(mktemp /tmp/requirements-XXXX.txt)
 
 # Note: To test lftools master branch change the lftools configuration below in
 #       the requirements file from "lftools[openstack]~=#.##.#" to
 #       git+https://github.com/lfit/releng-lftools.git#egg=lftools[openstack]
 
-cat << EOF > "$REQUIREMENTS_FILE"
+echo "Generating Requirements File"
+cat << 'EOF' > "$requirements_file"
 lftools[openstack]~=0.23.1
 python-heatclient~=1.16.1
 python-openstackclient~=3.16.0
@@ -25,12 +26,25 @@ dogpile.cache~=0.6.8  # Version 0.7.[01] seems to break openstackclient
 niet~=1.4.2 # Extract values from yaml
 EOF
 
-echo "Requirements file"
-echo "-----------------"
-cat "$REQUIREMENTS_FILE"
-
 # Use `python -m pip` to ensure we are using the latest version of pip
 python -m pip install --user --quiet --upgrade pip
 python -m pip install --user --quiet --upgrade setuptools
-python -m pip install --user --quiet --upgrade -r "$REQUIREMENTS_FILE"
-pip freeze
+python -m pip install --user --quiet --upgrade -r "$requirements_file"
+rm -rf $requirements_file
+
+# Generate a list of 'pip' packages before/after
+# After the 'after' list hase been created, perform a diff on the two lists
+echo "Listing pip packages"
+pip_list_file1="/tmp/pip-list-start.txt"
+pip_list_file2="/tmp/pip-list-end.txt"
+if [[ -f $pip_list_file1 ]]; then
+    pip list > $pip_list_file2
+    echo "Compare pip packages before/after..."
+    if diff --suppress-common-lines $pip_list_file1 $pip_list_file2; then
+        echo "No diffs"
+    fi
+    rm -rf $pip_list_file1 $pip_list_file2
+else
+    pip list > $pip_list_file1
+fi
+
