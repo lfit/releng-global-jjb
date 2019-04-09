8de34588e620c395514a15be962404f266b2c33f
Tim Johnson <tijohnson@linuxfoundation.org>
2019-04-09 14:41:20 -0700

    Reduce output in log files
    Previously ran 'pip freeze' (> log) twice to get list of pip packages
    installed dureing build.  We now runs 'pip list' (> file) twice and
    archives the files and the diffs. Configured 'logs-deploy.sh' to run in
    separate Jekins script.
    
    Issue: RELENG-1902
    Change-Id: Ib0f99bd1eb125015467c7eaf91d26c76e92bffa0
    Signed-off-by: Tim Johnson <tijohnson@linuxfoundation.org>

--- shell/python-tools-install.sh
+++ shell/python-tools-install.sh
@@ -10,15 +10,40 @@
 ##############################################################################
 echo "---> python-tools-install.sh"
 
-set -eux -o pipefail
+set -eu -o pipefail
 
-REQUIREMENTS_FILE=$(mktemp /tmp/requirements-XXXX.txt)
+# Generate a list of 'pip' packages pre-build/post-build
+# During post-build, perform a diff on the two lists and copy files to archive directory
+echo "Listing pip packages"
+pip_list_pre=/tmp/pip-list-pre.txt
+pip_list_post=/tmp/pip-list-post.txt
+pip_list_diffs=/tmp/pip-list-diffs.txt
+if [[ -f $pip_list_pre ]]; then
+    pip list > $pip_list_post
+    echo "Compare pip packages before/after..."
+    if diff --suppress-common-lines $pip_list_pre $pip_list_post \
+            | tee $pip_list_diffs; then
+        echo "No diffs" | tee $pip_list_diffs
+    fi
+    mkdir -p $WORKSPACE/archives
+    cp $pip_list_pre $pip_list_post $pip_list_diffs $WORKSPACE/archives
+    rm -rf $pip_list_pre $pip_list_post $pip_list_diffs
+    ls $WORKSPACE/archives
+    # Would just like to 'exit 0' here but we can't because the
+    # log-deploy.sh script is 'appended' to this file and it would not
+    # be executed.
+else
+    pip list > $pip_list_pre
+    # These 'pip installs' only need to be executed during pre-build
 
-# Note: To test lftools master branch change the lftools configuration below in
-#       the requirements file from "lftools[openstack]~=#.##.#" to
-#       git+https://github.com/lfit/releng-lftools.git#egg=lftools[openstack]
+    requirements_file=$(mktemp /tmp/requirements-XXXX.txt)
 
-cat << EOF > "$REQUIREMENTS_FILE"
+    # Note: To test lftools master branch change the lftools configuration below in
+    #       the requirements file from "lftools[openstack]~=#.##.#" to
+    #       git+https://github.com/lfit/releng-lftools.git#egg=lftools[openstack]
+
+    echo "Generating Requirements File"
+    cat << 'EOF' > "$requirements_file"
 lftools[openstack]~=0.23.1
 python-heatclient~=1.16.1
 python-openstackclient~=3.16.0
@@ -26,12 +51,9 @@ dogpile.cache~=0.6.8  # Version 0.7.[01] seems to break openstackclient
 niet~=1.4.2 # Extract values from yaml
 EOF
 
-echo "Requirements file"
-echo "-----------------"
-cat "$REQUIREMENTS_FILE"
-
-# Use `python -m pip` to ensure we are using the latest version of pip
-python -m pip install --user --quiet --upgrade pip
-python -m pip install --user --quiet --upgrade setuptools
-python -m pip install --user --quiet --upgrade -r "$REQUIREMENTS_FILE"
-pip freeze
+    # Use `python -m pip` to ensure we are using the latest version of pip
+    python -m pip install --user --quiet --upgrade pip
+    python -m pip install --user --quiet --upgrade setuptools
+    python -m pip install --user --quiet --upgrade -r "$requirements_file"
+    rm -rf $requirements_file
+fi
