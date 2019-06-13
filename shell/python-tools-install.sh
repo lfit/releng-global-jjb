b44ae6157fda5882fb0b5961540f6d09dd9a3aaf
Anil Belur <abelur@linuxfoundation.org>
2019-06-14 07:52:10 +1000

    Update lftools version to **v0.25.0**
    Change-Id: Ie72f9250e94c84b94b94f7e68f3fe8eb5dc4ef76
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- shell/python-tools-install.sh
+++ shell/python-tools-install.sh
@@ -44,7 +44,7 @@ else
 
     echo "Generating Requirements File"
     cat << 'EOF' > "$requirements_file"
-lftools[openstack]~=0.24.0
+lftools[openstack]~=0.25.0
 python-heatclient~=1.16.1
 python-openstackclient~=3.16.0
 dogpile.cache~=0.6.8  # Version 0.7.[01] seems to break openstackclient
