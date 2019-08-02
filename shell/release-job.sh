3ea1116652f0a896eebe82128dbc2942dd3a8876
Anil Belur <abelur@linuxfoundation.org>
2019-08-02 19:34:31 +1000

    Handle multiple repos listed in staging-repo.
    Handle multiple repos listed in staging-repo for ODL.
    
    Issue: RELENG-2129
    Change-Id: I55dc0a4022f93a6c3c0f389c04c88c38c53eff64
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- shell/release-job.sh
+++ shell/release-job.sh
@@ -46,11 +46,6 @@ for release_file in $release_files; do
     echo "--> Verifying $release_file schema."
     lftools schema verify $release_file release-schema.yaml
 
-    #OPTIONAL
-    if grep -q "\.maven_central_url" "$release_file"; then
-        MAVEN_CENTRAL_URL="$(niet ".maven_central_url" "$release_file")"
-    fi
-
     VERSION="$(niet ".version" "$release_file")"
     PROJECT="$(niet ".project" "$release_file")"
     LOG_DIR="$(niet ".log_dir" "$release_file")"
@@ -59,67 +54,75 @@ for release_file in $release_files; do
     LOGS_URL="${LOGS_SERVER}/${NEXUS_PATH}${LOG_DIR}"
     PATCH_DIR="$(mktemp -d)"
 
-    pushd "$PATCH_DIR"
-        wget --quiet "${LOGS_URL}"staging-repo.txt.gz
-        STAGING_REPO="$(zcat staging-repo.txt)"
+    wget --quiet "${LOGS_URL}"staging-repo.txt.gz
+    for staging_url in $(zcat staging-repo.txt.gz | awk -e '{print $2}'); do
+        # extract the domain name from URL
+        NEXUS_URL=$(echo $staging_url | sed -e 's|^[^/]*//||' -e 's|/.*$||')
+        # extract the staging repo from URL
+        STAGING_REPO=${staging_url#*repositories/}
+        echo "Merge will run"
+        echo "lftools nexus release --server $NEXUS_URL $STAGING_REPO"
+    done
 
-        #INFO
-        echo "INFO:"
-        echo "RELEASE_FILE: $release_file"
-        echo "LOGS_SERVER: $LOGS_SERVER"
-        echo "NEXUS_URL: $NEXUS_URL"
-        echo "NEXUS_PATH: $NEXUS_PATH"
-        echo "NEXUSPROXY: $NEXUSPROXY"
-        echo "JENKINS_HOSTNAME: $JENKINS_HOSTNAME"
-        echo "SILO: $SILO"
-        echo "PROJECT: $PROJECT"
-        echo "STAGING_REPO: $STAGING_REPO"
-        echo "VERSION: $VERSION"
-        echo "PROJECT: $PROJECT"
-        echo "LOG DIR: $LOG_DIR"
+    #INFO
+    echo "INFO:"
+    echo "RELEASE_FILE: $release_file"
+    echo "LOGS_SERVER: $LOGS_SERVER"
+    echo "NEXUS_URL: $NEXUS_URL"
+    echo "NEXUS_PATH: $NEXUS_PATH"
+    echo "NEXUSPROXY: $NEXUSPROXY"
+    echo "JENKINS_HOSTNAME: $JENKINS_HOSTNAME"
+    echo "SILO: $SILO"
+    echo "PROJECT: $PROJECT"
+    echo "VERSION: $VERSION"
+    echo "PROJECT: $PROJECT"
+    echo "LOG DIR: $LOG_DIR"
 
-        wget --quiet  "${LOGS_URL}"/patches/{"${PROJECT}".bundle,taglist.log.gz}
-        gunzip taglist.log.gz
-        cat "$PATCH_DIR"/taglist.log
-    popd
+   pushd "$PATCH_DIR"
+       wget --quiet  "${LOGS_URL}"/patches/{"${PROJECT}".bundle,taglist.log.gz}
+       gunzip taglist.log.gz
+       cat "$PATCH_DIR"/taglist.log
+   popd
 
-    # Verify allowed versions
-    # Allowed versions are "v#.#.#" or "#.#.#" aka SemVer
-    allowed_version_regex="^((v?)([0-9]+)\.([0-9]+)\.([0-9]+))$"
-    if [[ ! $VERSION =~ $allowed_version_regex ]]; then
-        echo "The version $VERSION is not a semantic valid version"
-        echo "Allowed versions are "v#.#.#" or "#.#.#" aka SemVer"
-        echo "See https://semver.org/ for more details on SemVer"
-        exit 1
-    fi
+   # Verify allowed versions
+   # Allowed versions are "v#.#.#" or "#.#.#" aka SemVer
+   allowed_version_regex="^((v?)([0-9]+)\.([0-9]+)\.([0-9]+))$"
+   if [[ ! $VERSION =~ $allowed_version_regex ]]; then
+       echo "The version $VERSION is not a semantic valid version"
+       echo "Allowed versions are "v#.#.#" or "#.#.#" aka SemVer"
+       echo "See https://semver.org/ for more details on SemVer"
+       exit 1
+   fi
 
-    git checkout "$(awk '{print $NF}' "$PATCH_DIR/taglist.log")"
-    git fetch "$PATCH_DIR/$PROJECT.bundle"
-    git merge --ff-only FETCH_HEAD
-    git tag -am "$PROJECT $VERSION" "$VERSION"
-    sigul --batch -c "$SIGUL_CONFIG" sign-git-tag "$SIGUL_KEY" "$VERSION" < "$SIGUL_PASSWORD"
+   git checkout "$(awk '{print $NF}' "$PATCH_DIR/taglist.log")"
+   git fetch "$PATCH_DIR/$PROJECT.bundle"
+   git merge --ff-only FETCH_HEAD
+   git tag -am "$PROJECT $VERSION" "$VERSION"
+   sigul --batch -c "$SIGUL_CONFIG" sign-git-tag "$SIGUL_KEY" "$VERSION" < "$SIGUL_PASSWORD"
 
-    echo "Showing latest signature for $PROJECT:"
-    gpg --import /tmp/onap-pubkey
-    echo "git tag -v "$VERSION""
-    git tag -v "$VERSION"
+   echo "Showing latest signature for $PROJECT:"
+   gpg --import /tmp/onap-pubkey
+   echo "git tag -v "$VERSION""
+   git tag -v "$VERSION"
 
+   ########## Merge Part ##############
+   if [[ "$JOB_NAME" =~ "merge" ]]; then
+       echo "Running merge"
+       gerrit_ssh=$(echo "$GERRIT_URL" | awk -F"/" '{print $3}')
+       git remote set-url origin ssh://"$RELEASE_USERNAME"@"$gerrit_ssh":29418/$PROJECT
+       git config user.name "$RELEASE_USERNAME"
+       git config user.email "$RELEASE_EMAIL"
+       git push origin "$VERSION"
 
-    ########## Merge Part ##############
-    if [[ "$JOB_NAME" =~ "merge" ]]; then
-        echo "Running merge"
-        gerrit_ssh=$(echo "$GERRIT_URL" | awk -F"/" '{print $3}')
-        git remote set-url origin ssh://"$RELEASE_USERNAME"@"$gerrit_ssh":29418/$PROJECT
-        git config user.name "$RELEASE_USERNAME"
-        git config user.email "$RELEASE_EMAIL"
-        git push origin "$VERSION"
-        lftools nexus release --server "$NEXUS_URL" "$STAGING_REPO"
-        if [ "${MAVEN_CENTRAL_URL}" == 'None' ]; then
-            echo "No Maven central url specified, not pushing to maven central"
-        else
-            lftools nexus release --server "$MAVEN_CENTRAL_URL" "$STAGING_REPO"
-        fi
-    fi
+       for staging_url in $(zcat staging-repo.txt.gz | awk -e '{print $2}'); do
+           # extract the domain name from URL
+           NEXUS_URL=$(echo $staging_url | sed -e 's|^[^/]*//||' -e 's|/.*$||')
+           # extract the staging repo from URL
+           STAGING_REPO=${staging_url#*repositories/}
+           echo "Promoting $STAGING_REPO on $NEXUS_URL."
+           lftools nexus release --server "$NEXUS_URL" "$STAGING_REPO"
+       done
 
+   fi
 done
 echo "########### End Script release-job.sh ###################################"
