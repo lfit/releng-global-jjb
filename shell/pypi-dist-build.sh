a226b4d52efbdb0b28296899ea2a833cb4fb6991
Lott, Christopher (cl778h) <cl778h@att.com>
2019-08-21 10:54:25 -0400

    Lift PyPI templates to Global-JJB from Acumos
    New templates provide features to build and push Python
    source and binary distributions to a PyPI repository.
    Check version string and push a tag on release.
    
    New job groups:
        {project-name}-gerrit-pypi-jobs
        {project-name}-github-pypi-jobs
    
    New templates:
        {project-name}-pypi-verify-{stream}
        gerrit-pypi-verify
        github-pypi-verify
        {project-name}-pypi-merge-{stream}
        gerrit-pypi-merge
        github-pypi-merge
        {project-name}-pypi-release-{stream}
        gerrit-pypi-release
        github-pypi-release
    
    Change-Id: If74925623f2a5804de8684b9a9933573dfea29a1
    Issue-Id: RELENG-2317
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

---
+++ shell/pypi-dist-build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> pypi-dist-build.sh"
+
+# Script to create Python source and binary distributions
+# Requires project file "setup.py"
+
+# Ensure we fail the job if any steps fail.
+# DO NOT set -u as virtualenv's activate script has unbound variables
+set -e -x -o pipefail
+
+if [ -d "/opt/pyenv" ]; then
+    echo "---> Setting up pyenv"
+    export PYENV_ROOT="/opt/pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+fi
+
+pip install wheel
+bdist=""
+if [[ "$BUILD_BDIST_WHEEL" = "true" ]]; then
+    bdist="bdist_wheel"
+fi
+python setup.py sdist $bdist
