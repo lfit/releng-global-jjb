e8e0f69a3a931fd81f537a3b903e510744ac1316
Lott, Christopher (cl778h) <cl778h@att.com>
2019-08-21 10:54:25 -0400

    Lift PyPI templates to Global-JJB from Acumos
    New templates provide features to build and push Python source and
    binary distributions to a PyPI server. New templates:
        {project-name}-pypi-merge
        gerrit-pypi-merge
        github-pypi-merge
        {project-name}-pypi-release
        gerrit-pypi-release
        github-pypi-release
    
    Change-Id: If74925623f2a5804de8684b9a9933573dfea29a1
    Issue-Id: RELENG-2317
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

---
+++ shell/pypi-publish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> pypi-publish.sh"
+
+# Script to publish Python distributions from a folder
+# to the PyPI defined in environment variable
+# Uses -l to get login shell path definitions
+
+# Ensure we fail the job if any steps fail.
+# DO NOT set -u as virtualenv's activate script has unbound variables
+set -e -x -o pipefail
+
+virtualenv /tmp/v/twine
+source "/tmp/v/twine/bin/activate"
+pip install twine
+twine upload -r $PYPI_SERVER dist/*
