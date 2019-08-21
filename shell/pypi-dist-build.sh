07b73b1bac5edebd3dde2f0149e15ec1dc8fecf1
Lott, Christopher (cl778h) <cl778h@att.com>
2019-08-21 10:54:25 -0400

    Lift PyPI templates to Global-JJB from Acumos
    New templates provide features to build and push Python
    source and binary distributions to a PyPI repository.
    Check version string and push a tag on release.
    
    New job groups:
        {project-name}-gerrit-pypi-jobs
        {project-name}-github-pypi-jobs
    
    New templates:
        {project-name}-pypi-verify-{stream}
        gerrit-pypi-verify
        github-pypi-verify
        {project-name}-pypi-merge-{stream}
        gerrit-pypi-merge
        github-pypi-merge
        {project-name}-pypi-release-verify-{stream}
        gerrit-pypi-release-verify
        github-pypi-release-verify
        {project-name}-pypi-release-merge-{stream}
        gerrit-pypi-release-merge
        github-pypi-release-merge
    
    Change-Id: If74925623f2a5804de8684b9a9933573dfea29a1
    Issue-Id: RELENG-2317
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

---
+++ shell/pypi-dist-build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> pypi-dist-build.sh"
+
+# Script to create Python source and binary distributions
+# Requires project file "setup.py"
+
+# Ensure we fail the job if any steps fail.
+# DO NOT set -u as virtualenv's activate script has unbound variables
+set -e -x -o pipefail
+
+cd "$WORKSPACE/$TOX_DIR"
+
+virtualenv "/tmp/pypi"
+# shellcheck source=/tmp/pypi/bin/activate disable=SC1091
+source "/tmp/pypi/bin/activate"
+
+bdist=""
+if $BUILD_BDIST_WHEEL; then
+    pip install wheel
+    bdist="bdist_wheel"
+fi
+python setup.py sdist $bdist
