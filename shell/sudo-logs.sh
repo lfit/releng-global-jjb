da2c59fae05a780a7ecb41acc5edb1b746087c52
Tim Johnson <tijohnson@linuxfoundation.org>
2019-09-09 10:51:54 -0700

    Capture sudo log on builders
    Created new script sudo-logs.sh to archive the sudo logs
    
    Issue: RELENG-1994
    Change-Id: I86a44e6abf18e35c7dc0e1840797e0dde5c764fb
    Signed-off-by: Tim Johnson <tijohnson@linuxfoundation.org>

---
+++ shell/sudo-logs.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> sudo-logs.sh"
+
+set -eu -o pipefail -o noglob
+
+# Copy/Generate 'sudo' log and copy to archive directory
+function copy_log()
+{
+    case $os in
+        fedora|centos|redhat|ubuntu|debian)
+            if ! sudo cp $sudo_log /tmp; then
+                echo "Unable to archive 'sudo' logs ($sudo_log)"
+                return
+            fi
+            ;;
+        suse)
+            # Do I need 'sudo' to run 'journalctl'?
+            journalctl | grep sudo > $sudo_log
+            ;;
+        *)  echo "Unexpected 'operatingsystem': $os"
+            exit 1
+            ;;
+    esac
+    sudo_log=$(basename $sudo_log)
+    sudo chown jenkins:jenkins /tmp/$sudo_log
+    chmod 0644 /tmp/$sudo_log
+    mkdir -p $WORKSPACE/archives/sudo
+    mv /tmp/$sudo_log $WORKSPACE/archives/sudo/$sudo_log
+
+}    # End copy_log()
+
+echo "Archiving 'sudo' log.."
+os=$(facter operatingsystem | tr '[:upper:]' '[:lower:]')
+case $os in
+    fedora|centos|redhat) sudo_log=/var/log/secure   ;;
+    ubuntu|debian)        sudo_log=/var/log/auth.log ;;
+    suse)                 sudo_log=/tmp/sudo.log     ;;
+    *)  echo "Unexpected 'operatingsystem': $os"
+        exit 1
+        ;;
+esac
+
+copy_log
