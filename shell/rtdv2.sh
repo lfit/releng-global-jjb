f998ee42e50ba342fdc12654b55a1fd599401381
Aric Gardner <agardner@linuxfoundation.org>
2019-09-09 14:25:50 -0400

    Readthedocs v2 jobs
    Global job that triggers on any docs changes.
    Creates docs project in rtd if absent on verify.
    Triggers docs build on merge
    
    fix tox job to push docs-dir to archives
    so that we can see generated docs
    
    ISSUE: RELENG-2362
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>
    Change-Id: Id6d85068eb068301b91d4deb74490e76381a7e54

---
+++ shell/rtdv2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+#WIP STUFF, this will be covered by python-tools-install.sh tox-install.sh tox-run.sh
+python3 -m venv ~/.venv
+source ~/.venv/bin/activate
+pip install --upgrade pip
+pip install yq
+
+git clone "https://gerrit.linuxfoundation.org/infra/releng/lftools"
+pushd lftools
+pip install -e .
+popd
+
+echo "---> rtd-verify.sh"
+set -xe -o pipefail
+
+echo "$PROJECT"
+project_dashed="${PROJECT////-}"
+umbrella="$(echo "$GERRIT_URL" | awk -F"." '{print $2}')"
+
+if [[ "$JOB_NAME" =~ "verify" ]]; then
+
+  # This retuns null if project exists.
+  if [[ "$(lftools rtd project-details "$umbrella"-"$project_dashed"-test | yq -r '.detail')" == "Not found." ]]; then
+    echo "Project not found, creating project"
+    lftools rtd project-create "$umbrella"-"$project_dashed-test" "$GERRIT_URL/$PROJECT" git "https://$umbrella-$project_dashed-test.readthedocs.io" py en
+
+  fi
+
+fi
+
+echo "Merge will run"
+echo "lftools rtd project-build-trigger $umbrella-$project_dashed-test $STREAM"
+
+if [[ "$JOB_NAME" =~ "merge" ]]; then
+    lftools rtd project-build-trigger "$umbrella"-"$project_dashed-test" "$STREAM"
+fi
