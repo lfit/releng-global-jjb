8f38688758f7218598e4b0a3fa67bdf8bdc5f4f5
Aric Gardner <agardner@linuxfoundation.org>
2019-09-09 14:25:50 -0400

    Readthedocs v2 jobs
    This change will need a new release of
    lftools before It will work.
    
    Global job that triggers on any docs changes.
    Creates docs project in rtd if absent on verify.
    Creates subproject association if project is not
    Jenkins global var MASTER_RTD_PROJECT
    Triggers docs build on merge
    
    fix tox job to push docs-dir to archives
    so that we can see generated docs
    
    ISSUE: RELENG-2362
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>
    Change-Id: Id6d85068eb068301b91d4deb74490e76381a7e54

---
+++ shell/rtdv2.sh
@@ -0,0 +1,68 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> rtd-verify.sh"
+set -euo pipefail
+
+project_dashed="${PROJECT////-}"
+umbrella="$(echo "$GERRIT_URL" | awk -F"." '{print $2}')"
+if [[ "$SILO" == "sandbox" ]]; then
+  rtdproject="$umbrella-$project_dashed-test"
+else
+  rtdproject="$umbrella-$project_dashed"
+fi
+
+#MASTER_RTD_PROJECT as a global jenkins var
+masterproject="$umbrella-$MASTER_RTD_PROJECT"
+
+echo "INFO:"
+echo "Project: $PROJECT"
+echo "Read the Docs Project: $rtdproject"
+echo "Read the Docs master Project: $masterproject"
+
+
+if [[ "$JOB_NAME" =~ "verify" ]]; then
+
+  # This retuns null if project exists.
+  if [[ "$(lftools rtd project-details "$rtdproject" | yq -r '.detail')" == "Not found." ]]; then
+    echo "Project not found, creating project https://$rtdproject.readthedocs.io"
+    lftools rtd project-create "$rtdproject" "$GERRIT_URL/$PROJECT" git "https://$rtdproject.readthedocs.io" py en
+  else
+    echo "Project exists in read the docs as https://$rtdproject.readthedocs.io"
+
+  fi
+
+fi
+
+echo "Merge will run"
+echo "lftools rtd project-build-trigger $rtdproject $STREAM"
+
+if [[ "$JOB_NAME" =~ "merge" ]]; then
+
+  if [[ "$rtdproject" != "$masterproject" ]]; then
+    subproject_exists=false
+    while read -r subproject; do
+        if [[ "$subproject" == "$rtdproject" ]]; then
+          subproject_exists=true
+          break
+        fi
+    done < <(lftools rtd subproject-list "$masterproject")
+
+    if $subproject_exists; then
+      echo "subproject relationship already created"
+    else
+      echo "Need to create subproject relationship"
+      lftools rtd subproject-create "$masterproject" "$rtdproject"
+      sleep 10
+    fi
+  fi
+
+  lftools rtd project-build-trigger "$rtdproject" "$STREAM"
+fi
