fc38a9260547c96d97c8856defebea672eedac3d
Tim Johnson <tijohnson@linuxfoundation.org>
2019-09-30 10:12:33 -0700

    Global JJB use python3 versions of lftools and jjb
    Move several Python venv's from python2 to python3. Remove all
    dependencies on jjb-install.sh & jjb-cleanup.sh.
    
    Issue: RELENG-2411
    Change-Id: I33d64627ae2ec4393f819ec596ec1b6db93e3fdf
    Signed-off-by: Tim Johnson <tijohnson@linuxfoundation.org>

--- shell/jjb-merge-job.sh
+++ shell/jjb-merge-job.sh
@@ -15,4 +15,16 @@ workers="${JJB_WORKERS:-0}"
 # Ensure we fail the job if any steps fail.
 set -eu -o pipefail
 
+source ~jenkins/lf-env.sh
+
+# Create a Py Venv and install jenkins-job-builder
+venv=$(mktemp -d)
+python3 -m venv $venv
+lf-activate $venv
+pip install --quiet --upgrade pip
+pip install --quiet --upgrade jenkins-job-builder==$JJB_VERSION
+
 jenkins-jobs update --recursive --delete-old --workers "$workers" jjb/
+
+rm -rf $venv
+
