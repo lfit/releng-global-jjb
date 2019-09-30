72164a11b04319236111cf9b188232a39c8cc99b
Tim Johnson <tijohnson@linuxfoundation.org>
2019-09-30 09:08:39 -0700

    Global JJB use python3 versions of lftools and jjb
    Create LF bash 'library' and install in ~jenkins
    
    Issue: RELENG-2411
    Change-Id: I211c85b9823ca5582a263ec145797d4de9424e10
    Signed-off-by: Tim Johnson <tijohnson@linuxfoundation.org>

--- jenkins-init-scripts/init.sh
+++ jenkins-init-scripts/init.sh
@@ -25,4 +25,7 @@ fi
 
 # Create the jenkins user last so that hopefully we DO NOT have to deal with
 # guard files
-"$jjb_init_scripts/create-jenkins-user.sh"
+$jjb_init_scripts/create-jenkins-user.sh
+
+cp $jjb_init_scripts/lf-env.sh ~jenkins/
+ls ~jenkins
---
+++ jenkins-init-scripts/lf-env.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/no-execute
+
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# A library of functions for LF/Jenkins bash scripts. In the general case, in
+# general, do NOT set shell variables here. If you want to make a variable
+# available to multiple scripts, provide a function/alias that sets the
+# variable: 'function lf_set_foo() {foo=asdf;}'. Any scripts that need access to
+# the variable can call the 'set' function. This keeps the name-space polution
+# to a minimum.
+
+################################################################################
+# Functions
+################################################################################
+
+# Execute pip from 'user' venv
+function lf-pip() { /home/jenkins/.local/bin/pip "$@" ; }
+
+# Echo all arguments to stderr
+function lf-echoerr() { echo "$@" 1>&2; }
+
+# Function to safely evaluate booleans
+# If the boolean equal '' it is an Fatal Error
+# Note the case of boolean will be ignored (always mapped to lower-case)
+function lf-boolean()
+{
+    local bool
+    bool=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+    if [[ -z $bool ]] ; then
+        # Output script name & line number of call to function
+        lf-echoerr "ERROR: $(basename $0) line: ${BASH_LINENO[0]} : A boolean cannot be a empty string" >&2
+        exit 1
+    fi
+    $bool
+}
+
+# Function to activate a Python Virtual Environment
+# Just validate the path and add 'bin' to the path
+function lf-activate()
+{
+    local venv_path=$1
+    # Validate the path to a VENV
+    if [[ ! -d $venv_path/bin ]]; then
+        lf-echoerr "ERROR: Is '$venv_path' a Python Environment ?"
+        return 1
+    fi
+    PATH=$venv_path/bin:$PATH
+}
+
+################################################################################
+# Functions that assign Variables
+################################################################################
+
+# Although these variables are 'shell' variables, they need to be upper-case so
+# 'shellcheck' does check for 'used-before-set' when they are referenced.
+
+function lf_set_maven_options()
+{
+    # shellcheck disable=SC2034  # Disable 'unused-varible' check
+    MAVEN_OPTIONS=$(echo --show-version --batch-mode -Djenkins \
+        -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+        -Dmaven.repo.local=/tmp/r -Dorg.ops4j.pax.url.mvn.localRepository=/tmp/r)
+}
+
+################################################################################
+# Variables (exceptions to every rule)
+################################################################################
+
+# Although this variable is a 'shell' variable, it needs to be upper-case so
+# 'shellcheck' does check for 'used-before-set' when they are referenced.
+
+# I am not that this will work in all situations. For now at least, there is
+# currently no code that depends on it.
+
+# shellcheck disable=SC2034 # Disable 'unused-varible' check
+GLOBAL_JJB_ROOT=$WORKSPACE/global-jjb
