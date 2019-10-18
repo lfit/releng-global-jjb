5d0054ee2c3059dc4c5430bb5df74cd39c9e7abf
Tim Johnson <tijohnson@linuxfoundation.org>
2019-10-18 10:10:28 -0700

    Update lf-env.sh library
    Add new function lf-create-user-venv(). Cleanup/enhance
    comments. Applied fixes/enhancements to lf-boolean() &
    lf-activate(). None of these functions are being referenced yet. Updated
    .coafile to silence errors.
    
    Change-Id: I1bd00ada3d2a5c0cccff4521d97b0193d42335f2
    Signed-off-by: Tim Johnson <tijohnson@linuxfoundation.org>

--- jenkins-init-scripts/lf-env.sh
+++ jenkins-init-scripts/lf-env.sh
@@ -26,41 +26,92 @@ function lf-pip() { /home/jenkins/.local/bin/pip "$@" ; }
 # Echo all arguments to stderr
 function lf-echoerr() { echo "$@" 1>&2; }
 
-# Function to safely evaluate booleans
-# If the boolean equal '' it is an Fatal Error
-# Note the case of boolean will be ignored (always mapped to lower-case)
+# Function to safely evaluate booleans If the boolean equal '' it is a Fatal
+# Error. Note the case of boolean will be ignored (always mapped to lower-case)
+
 function lf-boolean()
 {
     if [[ $# != 1 ]]; then
         echo "lf-boolean(): Missing boolean operand"
-        exit 1
+        return 1
     fi
     local bool
     bool=$(echo "$1" | tr '[:upper:]' '[:lower:]')
     if [[ -z $bool ]] ; then
         # Output script name & line number of call to function
         lf-echoerr "ERROR: $(basename $0) line: ${BASH_LINENO[0]} : A boolean cannot be a empty string" >&2
-        exit 1
+        return 1
     fi
     $bool
 }
 
-# Function to activate a Python Virtual Environment
-# Just validate the path and add 'bin' to the path
+# Function to activate a Python Virtual Environment. Validate the path and add
+# 'bin' to the path. If no path is specified, default to '~/.local'
+
 function lf-activate()
 {
-    if [[ $# != 1 ]]; then
-        echo "lf-activate(): Missing path operand"
-        exit 1
+    local venv_path
+    if [[ $# == 0 ]]; then
+        venv_path=~/.local
+    elif [[ $# == 1 ]]; then
+        venv_path=$1
+    else
+        lf-echoerr "lf-activate(): ERROR: Only one dir expected"
+        return 1
     fi
-    local venv_path=$1
     # Validate the path to a VENV
     if [[ ! -d $venv_path/bin ]]; then
         lf-echoerr "ERROR: Is '$venv_path' a Python Environment ?"
         return 1
     fi
+    echo "lf-activate(): Added $venv_path to PATH"
     PATH=$venv_path/bin:$PATH
 }
+
+# Create 'user' Python Virtual Environment (~/.local) This function may be
+# called multiple times within a build so if the venv has already been created
+# just quietly return. This function assumes that 'bash -e' is set.
+
+function lf-create-user-venv()
+{
+    local venv=~/.local
+    local requirements_file=/tmp/lf-requirements_file$$
+    local done_file=$venv/.lf-done
+    local pip_log=/tmp/pip.log$$
+
+    if [[ -f $done_file ]]; then
+        echo "User Venv Already Exists"
+        return
+    fi
+    if [[ -d $venv ]]; then
+        chmod -R +w $venv
+        rm -rf $venv
+    fi
+
+    echo "Installing Required Packages in the 'user' VENV"
+    cat << 'EOF' > $requirements_file
+git-review
+jenkins-job-builder
+lftools[openstack]
+niet
+python-heatclient
+python-openstackclient
+setuptools
+testresources
+tox
+yq
+EOF
+
+    python3 -m pip install --user -r "$requirements_file" pip > $pip_log
+    touch $done_file
+    #chmod -R -w $venv
+    # Archive the pip log. The 'pip' command can have 'version conflict' errors
+    # and it will still return '0' status, but the info will be in the log.
+    mkdir -p $WORKSPACE/archives
+    cp $pip_log $WORKSPACE/archives/pip-install.log
+    rm -rf $requirements_file $pip_log
+
+}   # End lf-create-user-venv()
 
 ################################################################################
 # Functions that assign Variables
