9267a88278e93dc829a79a59dd94e2050e817b98
Tim Johnson <tijohnson@linuxfoundation.org>
2019-10-20 19:41:42 -0700

    Update Bash Library
    Added lf-create-venv(). Updated behavior of lf-activate() to support
    lf-create-vent().  Fixes a same minor issues with existing functions and
    improved the comments.
    
    Change-Id: If50c9c282adb8b11176bbe6223578ea7edad1839
    Signed-off-by: Tim Johnson <tijohnson@linuxfoundation.org>

--- jenkins-init-scripts/lf-env.sh
+++ jenkins-init-scripts/lf-env.sh
@@ -26,41 +26,131 @@ function lf-pip() { /home/jenkins/.local/bin/pip "$@" ; }
 # Echo all arguments to stderr
 function lf-echoerr() { echo "$@" 1>&2; }
 
-# Function to safely evaluate booleans
-# If the boolean equal '' it is an Fatal Error
-# Note the case of boolean will be ignored (always mapped to lower-case)
+# Function to safely evaluate booleans If the boolean equal '' it is a Fatal
+# Error. Note the case of boolean will be ignored (always mapped to lower-case)
+
 function lf-boolean()
 {
     if [[ $# != 1 ]]; then
         echo "lf-boolean(): Missing boolean operand"
-        exit 1
+        return 1
     fi
     local bool
     bool=$(echo "$1" | tr '[:upper:]' '[:lower:]')
     if [[ -z $bool ]] ; then
         # Output script name & line number of call to function
         lf-echoerr "ERROR: $(basename $0) line: ${BASH_LINENO[0]} : A boolean cannot be a empty string" >&2
-        exit 1
+        return 1
     fi
     $bool
 }
 
-# Function to activate a Python Virtual Environment
-# Just validate the path and add 'bin' to the path
+# Function to activate a Python Virtual Environment. The argument is either a
+# path or a python executable. If the argument is a python executable check if
+# it is in the PATH. The python exeuctable name will be converted to a path
+# (~/.venv$version/bin) and then validated. If the argument is a path, the the
+# path ($path/bin) is validatedIf no path is specified, return 1.
+
 function lf-activate()
 {
     if [[ $# != 1 ]]; then
         echo "lf-activate(): Missing path operand"
-        exit 1
-    fi
-    local venv_path=$1
-    # Validate the path to a VENV
-    if [[ ! -d $venv_path/bin ]]; then
-        lf-echoerr "ERROR: Is '$venv_path' a Python Environment ?"
         return 1
     fi
-    PATH=$venv_path/bin:$PATH
+    local arg=$1
+    local venv_bin
+    if [[ $arg =~ ^python && $(type $arg) ]]; then
+        venv_bin=~/.venv${arg#python}/bin
+    else
+        venv_bin=$arg/bin
+    fi
+    # Validate the path to a VENV
+    if [[ ! -d $venv_bin ]]; then
+        lf-echoerr "ERROR: Is '$venv_bin' a Python Environment ?"
+        return 1
+    fi
+    echo "lf-activate: Adding $venv_bin to PATH"
+    PATH=$venv_bin:$PATH
 }
+
+# Create Python Virtual Environment for the specified version of python. This
+# function may be called multiple times so if the venv has already been created
+# just return. This function assumes that 'bash -e' is set.
+
+function lf-create-venv()
+{
+    if [[ $# != 1 ]]; then
+        lf-error "lf-create-user-venv(): ERROR: Missing Python argument"
+        return 1
+    fi
+    python=$1
+    if ! type $python ; then
+        lf-error "lf-create-user-venv(): ERROR: Unknow Python: $python"
+        return 1
+    fi
+    local venv=~/.venv${python#python}
+    local done_file=$venv/.lf-done
+    local suffix=$python-$$
+    local pip_log=/tmp/pip_log.$suffix
+    local req_list=/tmp/req_list.$suffix
+    local pkg_list=/tmp/pkg_list.$suffix
+    if [[ -f $done_file ]]; then
+        echo "Venv Already Exits: '$venv'"
+        return
+    fi
+    # Even if the 'done' files does not exist, make sure to clean up
+    if [[ -d $venv ]]; then
+        chmod -R +w $venv
+        rm -rf $venv
+    fi
+
+    echo "Installing Required Packages in the $venv"
+    cat << 'EOF' > $req_list
+git-review
+jenkins-job-builder
+lftools[openstack]
+niet
+python-heatclient
+python-openstackclient
+setuptools
+testresources
+tox
+yq
+EOF
+
+    case $python in
+    python2*)
+        lf-error "lf-create-user-venv(): ERROR: No support for: $python"
+        return 1
+        ;;
+    python3*)
+        $python -m venv $venv > $pip_log
+        $venv/bin/pip install --upgrade pip > $pip_log
+        $venv/bin/pip --version >> $pip_log
+        # Redirect errors for now
+        $venv/bin/pip install --upgrade -r $req_list >> $pip_log 2> /dev/null
+        $venv/bin/pip freeze | awk -F '=' '{print $1}' > $pkg_list
+        # Sometimes need to run twice to get all versions correct.
+        if $venv/bin/pip install --upgrade -r $pkg_list >> $pip_log 2>&1 > /dev/null ; then
+            echo "Run 'pip upgrade' to validate"
+            $venv/bin/pip install --upgrade -r $pkg_list >> $pip_log
+        fi
+        ;;
+    *)
+        lf-error "lf-create-user-venv(): ERROR: No support for: $python"
+        return 1
+        ;;
+    esac
+
+    touch $done_file
+    # Once this venv is created, make it read-only
+    chmod -R -w $venv
+    # Archive output of 'pip freeze'
+    mkdir -p $WORKSPACE/archives
+    $venv/bin/pip freeze > $WORKSPACE/archives/freeze-log-$python
+    rm -rf $pip_log $pkg_list $req_list
+
+}   # End lf-create-venv()
 
 ################################################################################
 # Functions that assign Variables
