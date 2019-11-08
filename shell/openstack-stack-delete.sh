7c025fd6e0eb9cf31ba7872c6af4f4c9b7d10a9a
Tim Johnson <tijohnson@linuxfoundation.org>
2019-11-08 09:11:01 -0800

    Get build cost estimates
    Added three new scripts: build-cost.sh, openstack-stack-cost.py &
    openstack-stack-delete.sh. Generate and archive files containing cost,
    time & job info. If this is a Stack Build call stack-cost.py to include
    costs from the Stack Members.
    
    Issue: RELENG-1867
    Change-Id: I658f5b42f90a28010e6f98f2f3c3ebb166192c30
    Signed-off-by: Tim Johnson <tijohnson@linuxfoundation.org>

---
+++ shell/openstack-stack-delete.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> openstack-stack-delete.sh"
+
+set -euf -o pipefail
+
+# shellcheck disable=SC1090
+source ~/lf-env.sh
+
+lf-activate-venv lftools[openstack] python-openstackclient
+
+echo "Retrieving stack cost for: $OS_STACK_NAME"
+stack_cost_script="/opt/ciman/global-jjb/shell/openstack-stack-cost.py"
+python --version
+if ! python $stack_cost_script $OS_STACK_NAME > stack-cost; then
+    # Continue on anyway
+    echo "WARNING: Unable to get stack costs, continuing anyway"
+    echo "total: Unknown" > stack-cost
+fi
+
+echo "Deleting $OS_STACK_NAME"
+lftools openstack --os-cloud "$OS_CLOUD" stack delete "$OS_STACK_NAME"
