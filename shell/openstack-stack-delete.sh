736c2fecc86e24e88c7c08556c554aaf9d154f07
Tim Johnson <tijohnson@linuxfoundation.org>
2019-11-08 09:11:01 -0800

    Get build cost estimates
    Generate and archive files containing cost, time & job info. If this is a
    Openstack job call 'lftools openstack stack cost' to retrieve and include costs
    from the stack members. Added two new scripts: openstack-stack-delete.sh &
    job-cost.sh. The 'openstack-stack-delete script' will retrieve the costs for the
    stack and delete the stack. The 'job-cost' will generate the job cost file
    which will include the costs for the build agent and any Openstack members.
    
    Issue: RELENG-1867
    Change-Id: I658f5b42f90a28010e6f98f2f3c3ebb166192c30
    Signed-off-by: Tim Johnson <tijohnson@linuxfoundation.org>

---
+++ shell/openstack-stack-delete.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> openstack-stack-delete.sh"
+
+set -eufo pipefail
+
+# shellcheck disable=SC1090
+source ~/lf-env.sh
+
+lf-activate-venv lftools[openstack] python-openstackclient
+
+echo "INFO: Retrieving stack cost for: $OS_STACK_NAME"
+if ! lftools openstack --os-cloud $OS_CLOUD stack cost $OS_STACK_NAME > stack-cost; then
+    echo "WARNING: Unable to get stack costs, continuing anyway"
+    echo "total: 0" > stack-cost
+else
+    echo "DEBUG: Successfully retrieved stack cost: $(cat stack-cost)"
+fi
+
+# Delete the stack even if the stack-cost script fails
+lftools openstack --os-cloud "$OS_CLOUD" stack delete "$OS_STACK_NAME" \
+    | echo "INFO: $(cat)"
+
