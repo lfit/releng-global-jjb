lfit/releng-global-jjb

commit fe2beb15bc9effb5b91ba17b520ab6c167dd6604

Fix OpenStack image validation for multi-cloud
While working on getting FDio setup to use the OpenStack cloud
configuration we found that the validation script does not operate
properly against a multi-OS cloud configuration

Change-Id: Idfe5ca2a060bc0f696cc1479e1ec3d40318e3c29
Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

shell/jenkins-verify-images.sh
@@ -16,21 +16,40 @@ set -eux -o pipefail
 
 error=false
 
-for file in jenkins-config/clouds/openstack/*/*; do
-  # Set the $IMAGE_NAME variable to the the file's IMAGE_NAME value
-  export "$(grep ^IMAGE_NAME= "$file")"
-  # The image should be listed as active
+verify_images()
+{
+  echo "Verifying images on $1"
+  for file in "$1"/*; do
+    # Set the $IMAGE_NAME variable to the the file's IMAGE_NAME value
+    export "$(grep ^IMAGE_NAME= "$file")"
+    # The image should be listed as active
 
-  if ! openstack image list --property name="$IMAGE_NAME" | grep "active"; then
-    echo "ERROR: No matching image found for $IMAGE_NAME"
-    error=true
-  fi
-  # Set the $HARDWARE_ID variable to the the file's HARDWARE_ID value
-  export "$(grep ^HARDWARE_ID= "$file")"
-  # The flavor should be listed. Spaces in grep string ensure complete match.
+    if ! openstack image list --property name="$IMAGE_NAME" | grep "active"; then
+      echo "ERROR: No matching image found for $IMAGE_NAME"
+      error=true
+    fi
+    # Set the $HARDWARE_ID variable to the the file's HARDWARE_ID value
+    export "$(grep ^HARDWARE_ID= "$file")"
+    # The flavor should be listed. Spaces in grep string ensure complete match.
 
-  if ! openstack flavor list | grep " $HARDWARE_ID "; then
-    echo "ERROR: No matching flavor found for $HARDWARE_ID"
+    if ! openstack flavor list | grep " $HARDWARE_ID "; then
+      echo "ERROR: No matching flavor found for $HARDWARE_ID"
+      error=true
+    fi
+  done
+}
+
+for cloud in jenkins-config/clouds/openstack/*; do
+  echo "Verifying that cloud has a master configuration file"
+  # Verify that we have a cloud config file
+  if [ -f "$cloud/cloud.cfg" ]; then
+    # Get the OS_CLOUD variable from cloud config and export it
+    OS_CLOUD=$(grep ^CLOUD_CREDENTIAL_ID= "$cloud/cloud.cfg" | cut -d'=' -f2)
+    export OS_CLOUD
+
+    verify_images "$cloud"
+  else
+    echo "ERROR: No cloud.cfg for $cloud"
     error=true
   fi
 done

jjb/lf-ci-jobs.yaml
@@ -408,7 +408,6 @@
     disable-job: false
     git-url: "$GIT_URL/$GERRIT_PROJECT"
     github-url: "https://github.com"
-    openstack-cloud: vex
     submodule-timeout: 10
     submodule-disable: false
 
@@ -466,8 +465,6 @@
 
     builders:
       - lf-infra-pre-build
-      - inject:
-          properties-content: OS_CLOUD={openstack-cloud}
       - shell: !include-raw-escape: ../shell/jenkins-verify-images.sh
 
     publishers:

releasenotes/notes/fix-multi-cloud-validator-d6edcb309e5f02e0.yaml
@@ -1,0 +1,13 @@
+---
+fixes:
+  - |
+    Multi-cloud image validation was not properly working as we force set the
+    OS_CLOUD environment variable before validation of images. This has been
+    rectified to dynamicaly modify the OS_CLOUD variable based upon the
+    cloud.cfg that will be used to define the cloud in Jenkins.
+  - |
+    We now validate that a cloud.cfg file exists for any defined OpenStack
+    cloud.
+  - |
+    Removes undocumented, and now unneeded, openstack-cloud variable from the
+    jenkins-cfg-verify job definition

commit 875c535be687201408955e883106156169170a19

Fix OS_CLOUD export for image validation
Change fe2beb15bc9effb5b91ba17b520ab6c167dd6604 introduced a bug where
the CLOUD_CREDENTIAL_ID was being used for the OS_CLOUD export variable.
This was done since the test for multi-cloud image validation had the
credential IDs matching the OS_CLOUD variable. However, most of our
systems have the CLOUD_CREDENTIAL_ID set to the default of 'os-cloud'
and therefore after pulling updating to a global-jjb with that change
would no longer be able to validate images!

We now look for an OS_CLOUD variable in the cloud.cfg file and use it if
it is set, otherwise we default to an OS_CLOUD of 'vex' as we previously
did before change fe2beb15bc9effb5b91ba17b520ab6c167dd6604

Change-Id: I0af763495fa3a7ce2e8c080324abaf5d384e5188
Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

shell/jenkins-verify-images.sh
@@ -44,7 +44,8 @@ for cloud in jenkins-config/clouds/openstack/*; do
   # Verify that we have a cloud config file
   if [ -f "$cloud/cloud.cfg" ]; then
     # Get the OS_CLOUD variable from cloud config and export it
-    OS_CLOUD=$(grep ^CLOUD_CREDENTIAL_ID= "$cloud/cloud.cfg" | cut -d'=' -f2)
+    os_cloud=$(grep ^OS_CLOUD= "$cloud/cloud.cfg" | cut -d'=' -f2)
+    OS_CLOUD="${os_cloud:-vex}"
     export OS_CLOUD
 
     verify_images "$cloud"