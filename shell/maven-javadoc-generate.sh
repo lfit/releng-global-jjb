0b66bcad537b3185683e45e36d36c27b559a0c9b
Lott, Christopher (cl778h) <cl778h@att.com>
2019-12-23 15:32:48 -0500

    Generate javadoc from a project in a subdirectory
    Lift the assumption that all files are in the git repository root.
    Extend maven-javadoc publish and javadoc-verify macros with mvn-dir
    configuration parameter (like tox-dir), defaults to '.' to preserve
    existing behavior.
    Extend maven-javadoc-generate.sh script to invoke mvn with the -f
    option and use the directory name when archiving the generated HTML.
    Document the new configuration parameter.
    
    Projects using macros lf-maven-javadoc-publish, lf-maven-javadoc-verify
    (i.e., using job templates gerrit-maven-javadoc-publish,
    github-maven-javadoc-publish, gerrit-maven-javadoc-verify or
    github-maven-javadoc-verify) must ensure the maven -f option is not
    used in config parameter mvn-opts or mvn-params.
    
    Change-Id: Ib02a12a3192b8be577a0367476628c46db333231
    Issue-ID: RELENG-1872
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

--- shell/maven-javadoc-generate.sh
+++ shell/maven-javadoc-generate.sh
@@ -21,6 +21,11 @@ mkdir -p "$WORKSPACE/archives"
 
 export MAVEN_OPTS
 
+# Warn if -f occurs in options or params but don't fail
+if [[ $MAVEN_OPTIONS =~ "-f" || $MAVEN_PARAMS =~ "-f" ]]; then
+    echo "WARNING: -f in MAVEN_OPTIONS or MAVEN_PARAMS conflicts with MAVEN_DIR"
+fi
+
 # Disable SC2086 because we want to allow word splitting for $MAVEN_* parameters.
 # shellcheck disable=SC2086
 $MVN clean install javadoc:aggregate \
@@ -30,9 +35,10 @@ $MVN clean install javadoc:aggregate \
     -Dfindbugs.skip=true \
     --global-settings "$GLOBAL_SETTINGS_FILE" \
     --settings "$SETTINGS_FILE" \
+    -f "$MAVEN_DIR" \
     $MAVEN_OPTIONS $MAVEN_PARAMS
 
-mv "$WORKSPACE/target/site/apidocs" "$JAVADOC_DIR"
+mv "$WORKSPACE/$MAVEN_DIR/target/site/apidocs" "$JAVADOC_DIR"
 
 # TODO: Nexus unpack plugin throws a "504 gateway timeout" for jobs archiving
 # large number of small files. Remove the workaround only we move away from
