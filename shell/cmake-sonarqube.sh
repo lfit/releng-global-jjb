4612cad3b3c8418ac5c71d0b363e7c3a91a69153
Lott, Christopher (cl778h) <cl778h@att.com>
2020-02-27 14:45:39 -0500

    Add templates Cmake + SonarQube Jenkins plugin
    New templates gerrit-cmake-sonarqube and github-cmake-sonarqube
    use the SonarQube Jenkins plug-in to analyze CXX code and publish
    the results.  Modeled after the generic gerrit/github-sonar templates.
    The new templates lift the limitations of the existing templates
    gerrit/github-cmake-sonar which download and install a Sonar scanner,
    and cannot report unit-test code coverage statistics.
    
    Change-Id: I2457ab0a40e226b8b5b2bbe18d2b9f5cde3281e1
    Issue-ID: RELENG-2760
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

---
+++ shell/cmake-sonarqube.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2020 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> cmake-sonarqube.sh"
+
+# Runs cmake then make using a wrapper for the SonarQube Jenkins plug-in
+# https://docs.sonarqube.org/latest/analysis/languages/cfamily/
+
+set -eux -o pipefail
+
+build_dir="${BUILD_DIR:-$WORKSPACE/build}"
+build_wrap_dir="${BUILD_WRAP_DIR:-$WORKSPACE/bw-output}"
+cmake_opts="${CMAKE_OPTS:-}"
+make_opts="${MAKE_OPTS:-}"
+
+cd /tmp || exit 1
+wget -q -O bw.zip https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
+unzip -q bw.zip
+sudo mv build-wrapper-* /opt/build-wrapper
+
+mkdir -p "$build_dir"
+cd "$build_dir" || exit 1
+
+# $cmake_opts needs to wordsplit to pass options.
+# shellcheck disable=SC2086
+eval cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" $cmake_opts ..
+
+# $make_opts may be empty.
+# shellcheck disable=SC2086
+/opt/build-wrapper/build-wrapper-linux-x86-64 --out-dir "$build_wrap_dir" make $make_opts
+
+echo "---> cmake-sonarqube.sh ends"
