00f44dfc848c456aea5d5773229cef1465db1f34
Lott, Christopher (cl778h) <cl778h@att.com>
2020-02-28 13:34:22 -0500

    Show details and detect errors in tox-run.sh
    Revise tox-run.sh to guard against unbound variable TOX_ENVS;
    stop on error or unbound variable (options -eu); and print
    commands before executing (option -x). Add echo command at end.
    Main effect is to show TOX_ENVS parameter value in the job log.
    
    Change-Id: Iad78123f32f5c32792ba5ec8584921066ff9af70
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

--- shell/tox-run.sh
+++ shell/tox-run.sh
@@ -10,12 +10,14 @@
 ##############################################################################
 echo "---> tox-run.sh"
 
+set -eux -o pipefail
+
 ARCHIVE_TOX_DIR="$WORKSPACE/archives/tox"
 ARCHIVE_DOC_DIR="$WORKSPACE/archives/docs"
 mkdir -p "$ARCHIVE_TOX_DIR"
 cd "$WORKSPACE/$TOX_DIR" || exit 1
 
-if [ -d "/opt/pyenv" ]; then
+if [[ -d /opt/pyenv ]]; then
     echo "---> Setting up pyenv"
     export PYENV_ROOT="/opt/pyenv"
     export PATH="$PYENV_ROOT/bin:$PATH"
@@ -25,8 +27,8 @@ if [ -d "/opt/pyenv" ]; then
 fi
 
 PARALLEL="${PARALLEL:-true}"
-if [ "${PARALLEL}" = true ]; then
-    if [ -n "$TOX_ENVS" ]; then
+if [[ ${PARALLEL} = true ]]; then
+    if [[ -n ${TOX_ENVS:-} ]]; then
         tox -e "$TOX_ENVS" --parallel auto --parallel-live | tee -a "$ARCHIVE_TOX_DIR/tox.log"
         tox_status="${PIPESTATUS[0]}"
     else
@@ -34,7 +36,7 @@ if [ "${PARALLEL}" = true ]; then
         tox_status="${PIPESTATUS[0]}"
     fi
 else
-    if [ -n "$TOX_ENVS" ]; then
+    if [[ -n ${TOX_ENVS:-} ]]; then
         tox -e "$TOX_ENVS" | tee -a "$ARCHIVE_TOX_DIR/tox.log"
         tox_status="${PIPESTATUS[0]}"
     else
@@ -43,6 +45,8 @@ else
     fi
 fi
 
+echo "---> Completed tox runs"
+
 # Disable SC2116 as we want to echo a space separated list of TOX_ENVS
 # shellcheck disable=SC2116
 for i in .tox/*/log; do
@@ -50,14 +54,13 @@ for i in .tox/*/log; do
     cp -r "$i" "$ARCHIVE_TOX_DIR/$tox_env"
 done
 
-echo "Completed tox runs."
-
-
 # If docs are generated push them to archives.
 DOC_DIR="${DOC_DIR:-docs/_build/html}"
-if [[ -d "$DOC_DIR" ]]; then
+if [[ -d $DOC_DIR ]]; then
     echo "---> Archiving generated docs"
     mv "$DOC_DIR" "$ARCHIVE_DOC_DIR"
 fi
+
+echo "---> tox-run.sh ends"
 
 test "$tox_status" -eq 0 || exit "$tox_status"
