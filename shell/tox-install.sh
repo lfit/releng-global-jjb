7b3a94680451f4bf5eb902ae63658d33d8029bdd
Aric Gardner <agardner@linuxfoundation.org>
2020-03-04 15:46:18 -0500

    Python 3.5 and python2 tox in Ubuntu 16.04
    Ubuntu 16.04 calls a python2 version of tox.
    so we must upgrade it even if we are testing with python3
    
    ubuntu 16.04 has python 3.5 we must pinn the zipp package
    
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>
    Change-Id: I4d2513c15baf4f00313885b35f8f0a297874b2b3

--- shell/tox-install.sh
+++ shell/tox-install.sh
@@ -14,12 +14,25 @@ echo "---> tox-install.sh"
 # Use -x to show value of $PYTHON in output
 set -eux -o pipefail
 
-# Tox version is pulled in through detox to mitigate version conflict
+#Python 3.5 and python2 tox in Ubuntu 16.04 workaround
+done="False"
+if [[ -f /etc/lsb-release ]]; then
+   # shellcheck disable=SC1091
+   source /etc/lsb-release
+   if [[ $DISTRIB_RELEASE == "16.04" ]]; then
+       echo "WARNING: Python projects should move to Ubuntu 18.04 to continue receiving support"
+       python2 -m pip install --user --quiet --upgrade tox tox-pyenv virtualenv more-itertools~=5.0.0
+       python3 -m pip install --user --quiet --upgrade tox tox-pyenv virtualenv zipp==1.1.0
+       done="True"
+   fi
+fi
 
-if [[ $PYTHON == "python2" ]]; then
-    $PYTHON -m pip install --user --quiet --upgrade tox tox-pyenv virtualenv more-itertools~=5.0.0
-else
-    $PYTHON -m pip install --user --quiet --upgrade tox tox-pyenv virtualenv
+if [[ $done != "True" ]]; then
+    if [[ $PYTHON == "python2" ]]; then
+        $PYTHON -m pip install --user --quiet --upgrade tox tox-pyenv virtualenv more-itertools~=5.0.0
+    else
+        $PYTHON -m pip install --user --quiet --upgrade tox tox-pyenv virtualenv
+    fi
 fi
 
 # installs are silent, show version details in log
