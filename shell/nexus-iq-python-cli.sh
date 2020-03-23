ed44b1b24eb8a9e8ae9c48100ebba820e4dd0d99
Lott, Christopher (cl778h) <cl778h@att.com>
2020-03-23 17:14:30 -0400

    Add Nexus IQ CLM templates for Python/tox
    Add new templates gerrit-tox-nexus-iq/github-tox-nexus-iq to
    analyze Python package requirements.  Replaces the old
    g*it-python-xc-clm jobs which this change marks as deprecated.
    Add script nexus-iq-python-cli.sh with correct invocation
    and arguments for the latest version of the Sonar IQ scanner.
    
    Change-Id: I5c34ad02e3e37a0e04dfa7e64b1013ebc26119fc
    Issue-ID: RELENG-2821
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

---
+++ shell/nexus-iq-python-cli.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2020 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> nexus-iq-python-cli.sh"
+# This script downloads the specified version of the nexus-iq-cli jar, uses it
+# to analyze the Python project dependencies from the specified requirements file,
+# then publishes the result to an LF server using the specified credentials.
+
+# stop on error or unbound variable
+set -eu
+# do not print commands, credentials should not be logged
+set +x
+# check if tox established the prereqs for this analysis
+if [[ ! -f $REQUIREMENTS_FILE ]]; then
+    echo "ERROR: failed to find file $REQUIREMENTS_FILE"
+    exit 1
+fi
+CLI_LOCATION="/tmp/nexus-iq-cli-${NEXUS_IQ_CLI_VERSION}.jar"
+echo "INFO: downloading nexus-iq-cli version $NEXUS_IQ_CLI_VERSION"
+wget -nv "https://download.sonatype.com/clm/scanner/nexus-iq-cli-${NEXUS_IQ_CLI_VERSION}.jar" -O "${CLI_LOCATION}"
+echo "-a" > cli-auth.txt
+echo "${NEXUS_IQ_USER}:${NEXUS_IQ_PASSWORD}" >> cli-auth.txt
+echo "INFO: running nexus-iq-cli on project $NEXUS_IQ_PROJECT_NAME and file $REQUIREMENTS_FILE"
+# result.json is a mystery
+java -jar "${CLI_LOCATION}" @cli-auth.txt -s https://nexus-iq.wl.linuxfoundation.org -i "${NEXUS_IQ_PROJECT_NAME}" -t build -r result.json "${REQUIREMENTS_FILE}"
+rm cli-auth.txt
+rm "${CLI_LOCATION}"
+
+echo "---> nexus-iq-python-cli.sh ends"
