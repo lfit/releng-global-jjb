d7ff9721558d957ad419b7334672f75f31269a1c
Lott, Christopher (cl778h) <cl778h@att.com>
2020-03-29 18:53:05 -0400

    Verify build nodes in YAML against config files
    Extend lf-jjb-verify anchor to call script jjb-verify-build-nodes.sh.
    Script checks build-node labels in YAML files within the jjb
    subdirectory against labels defined by config files in the jenkins
    configuration directory for minions.  Use a conditional build step
    guarded by existence of jenkins-config/clouds/openstack so the verify
    step runs on projects, not on global-jjb.
    
    Issue-ID: RELENG-2828
    Change-Id: Iaf315b4a68c3488b5ead204a57361a8bcbca91d7
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

---
+++ shell/jjb-verify-build-nodes.sh
@@ -0,0 +1,73 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2020 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> jjb-verify-build-nodes.sh"
+
+# Checks build-node labels used in ci-management job definitions
+# Requires -l to get $HOME/bin in path where yq is installed
+# Prereqs:
+# Python CLI yq is available; e.g., from python-tools-install.sh.
+# Working directory is a ci-management repo with a jjb subdir and
+# a jenkins-config/clouds/openstack subdir.
+
+set -eu -o pipefail
+
+# function to search an array for a value
+# $1 is value
+# $2 is array, passed via ${array[@]}
+isValueInArray () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+# discover build node labels
+declare -a labels=()
+suffix=".cfg"
+while IFS= read -r ; do
+    file="$REPLY"
+    # valid files contain IMAGE_NAME; skip the cloud config file
+    if grep -q "IMAGE_NAME" "$file" && ! grep -q "CLOUD_CREDENTIAL_ID" "$file"; then
+        # file name is a valid label, without path prefix and suffix
+        name=$(basename -s "$suffix" "$file")
+        labels+=("$name")
+        # a file can define a custom label
+        custom=$(grep "LABELS=" "$file" | cut -d= -f2)
+        if [[ -n $custom ]]; then
+            if ! isValueInArray "$custom" "${labels[@]}"; then
+                labels+=("$custom")
+            fi
+        fi
+    fi
+done < <(find "jenkins-config/clouds/openstack" -name \*$suffix)
+if (( ${#labels[@]} == 0 )); then
+    echo "ERROR: found no node config files"
+    exit 1
+fi
+echo "Found ${#labels[@]} build-node labels:"
+echo "${labels[@]}"
+
+# check build node label uses
+errs=0
+while IFS= read -r ; do
+    file="$REPLY"
+    echo "Checking $file"
+    nodes=$(yq 'recurse | ."build-node"? | values' "$file" | sort -u | tr -d '"')
+    for node in $nodes; do
+        if ! isValueInArray "$node" "${labels[@]}"; then
+            echo "ERROR: file $file uses unknown build-node $node"
+            errs=$((errs+1))
+        fi
+    done
+done < <(find "jjb" -name '*.yaml')
+
+echo "---> jjb-verify-build-nodes.sh ends"
+exit $errs
