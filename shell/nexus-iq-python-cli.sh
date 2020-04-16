8bb4a9c09f2b670d02c1fd026289591016f5d3ab
Aric Gardner <agardner@linuxfoundation.org>
2020-04-16 15:23:38 -0400

    Fix nexus-iq-cli.sh
    it is now python specific.
    generate a requirements.txt
    run the build without -xc and with -r results.json
    
    JIRA: IT-19271
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>
    Change-Id: Ida69a5fc6e27c19161019a7c3e9511b61a623e2a

--- shell/nexus-iq-python-cli.sh
+++ shell/nexus-iq-python-cli.sh
@@ -8,7 +8,7 @@
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
 ##############################################################################
-echo "---> nexus-iq-cli.sh"
+echo "---> nexus-iq-python-cli.sh"
 # This script downloads the specified version of the nexus-iq-cli jar, uses it
 # to perform an XC Evaluation or extended report which provides a scan of python
 # files within the repo starting at the root, then publishes the result to an LF
@@ -24,8 +24,13 @@ wget -nv "https://download.sonatype.com/clm/scanner/nexus-iq-cli-${NEXUS_IQ_CLI_
 echo "-a" > cli-auth.txt
 echo "${CLM_USER}:${CLM_PASSWORD}" >> cli-auth.txt
 echo "INFO: running nexus-iq-cli scan on project $CLM_PROJECT_NAME"
-java -jar "${CLI_LOCATION}" @cli-auth.txt -xc -i "${CLM_PROJECT_NAME}" -s https://nexus-iq.wl.linuxfoundation.org -t build .
+
+#Generate a requirements.txt
+source ~/lf-env.sh
+pip freeze > requirements.txt
+
+java -jar "${CLI_LOCATION}" @cli-auth.txt -i "${CLM_PROJECT_NAME}" -r result.json -s https://nexus-iq.wl.linuxfoundation.org -t build requirements.txt
 rm cli-auth.txt
 rm "${CLI_LOCATION}"
 
-echo "---> nexus-iq-cli.sh ends"
+echo "---> nexus-iq-python-cli.sh ends"
