10ac37b80e9e0327ad3e02631d9c2e5103b32ba9
Eric Ball <eball@linuxfoundation.org>
2020-12-09 08:19:51 -0800

    Add pipeline-verify jobs to lint and check agents
    This adds a simple pipeline verification job that will lint any
    pipelines found, and check to make sure that they are not set up to
    run on master.
    
    Issue: RELENG-2185
    Change-Id: I76d400d145857887a6d28d7c2a9869ec4acf414d
    Signed-off-by: Eric Ball <eball@linuxfoundation.org>

---
+++ shell/pipeline-linter.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2020 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> pipeline-linter.sh"
+
+set -eu -o pipefail
+
+JENKINS_CRUMB=`curl --silent "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)"`
+JENKINS_VAL="$JENKINS_URL/pipeline-model-converter/validate"
+JENKINS_FILE_LIST=(`grep -lr "^pipeline\s*{" vars src Jenkinsfile*`)
+exit_code=0
+
+for JENKINS_FILE in "${JENKINS_FILE_LIST[@]}"
+do
+    ret=$(curl --silent -X POST -H $JENKINS_CRUMB -F "jenkinsfile=<$JENKINS_FILE" $JENKINS_VAL)
+    if [[ $ret == *"Errors"* ]];then
+        echo "ERROR: Linting error for $JENKINS_FILE"
+        echo $ret
+        exit_code=1
+    # Check for the line "agent any". This includes master as an agent.
+    elif grep "^\s*agent any" "$JENKINS_FILE"; then
+        echo "ERROR: $JENKINS_FILE is set to use any agent. Please specify a label instead."
+        exit_code=1
+    # If "any" is not used, check for master as a specified label or node.
+    elif grep -Pz "agent\s*\{\s*(label|node)\s+['\"]*master" "$JENKINS_FILE"; then
+        echo "ERROR: $JENKINS_FILE is set to use master as an agent. Please specify a different label."
+        exit_code=1
+    else
+        echo "$JENKINS_FILE successfully validated"
+    fi
+done
+
+# Set non-zero exit if linter reports any errors
+exit $exit_code
