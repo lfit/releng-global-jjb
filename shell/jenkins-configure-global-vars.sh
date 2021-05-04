f21d1ab5c2c553951e9601a605ebc8018676432f
Guillaume Lambert <guillaume.lambert@orange.com>
2021-05-04 13:46:48 +0200

    Fix bashate E006 warnings
    ie.e lines too long > 120 characters
    
    Lines longer than 120 columns can not only cause problems when
    reading/writing code, but also often indicates a bad smell,
    e.g. too many levels of indentation due to overly complex functions
    which require refactoring into smaller chunks.
    
    Signed-off-by: Guillaume Lambert <guillaume.lambert@orange.com>
    Change-Id: Ibbb171353fd960ea93145f7913e62e327bd6a86b

--- shell/jenkins-configure-global-vars.sh
+++ shell/jenkins-configure-global-vars.sh
@@ -28,7 +28,8 @@ for silo in $silos; do
     if [ ! -f "$WORKSPACE/jenkins-config/global-vars-$silo.sh" ]; then
         echo "WARN: jenkins-config/global-vars-$silo.sh does not exist. Skipping cloud management..."
         echo "We highly recommend setting up global-vars-$silo.sh to manage the Jenkins global variables."
-        echo "Refer to https://docs.releng.linuxfoundation.org/projects/global-jjb/en/latest/jjb/lf-ci-jobs.html#global-environment-variables for details."
+        echo "Refer to https://docs.releng.linuxfoundation.org/projects/global-jjb/en/latest/jjb/lf-ci-jobs.html"\
+"#global-environment-variables for details."
         continue
     fi
 
