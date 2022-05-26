d5d10e7fbfb75cbbfbfbd399a77a0ff3148376bd
Anil Belur <abelur@linuxfoundation.org>
2022-05-27 09:58:32 +1000

    Fix: Sign artifacts on CentOS Stream 8/9
    Update the sigul-sign-dir.sh to sign artifacts using docker.
    The docker image is built on CentOS Streams 8/9. The newer version
    of sigul 1.1.1 available for CentOS 8 is not backwords compatible
    with the version of sigul on CentOS 7.
    
    As a temporary workaround build a CentOS7 docker image with
    sigul installed and use it for signing artificats on platforms
    where sigul is not readly available.
    
    Issue-ID: IT-23826
    Change-Id: Ie22e23240f7fe388219c0afc4d4c229f390efa9c
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- shell/sigul-sign-dir.sh
+++ shell/sigul-sign-dir.sh
@@ -13,4 +13,37 @@ echo "---> sigul-sign-dir.sh"
 # Ensure we fail the job if any steps fail.
 set -e -o pipefail
 
-lftools sign sigul -m "${SIGN_MODE}" "${SIGN_DIR}"
+# Download the Dockerfile and the enterpoint to build the docker image.
+wget -O "${WORKSPACE}/Dockerfile" "https://gist.githubusercontent.com/askb/"\
+"2feb911f9d18ba039344e697fba87b7b/raw/e95f6180f4bca303396bcef5e1109fc703574c40/Dockerfile"
+wget -O "${WORKSPACE}/sigul-sign.sh" "https://gist.githubusercontent.com/askb/"\
+"eab6b21b3127e9da2c780673da23ec81/raw/84b0a54fe67f365bb0ed7a5fb43e20e0721eae37/sigul-sign.sh"
+
+OS=$(facter operatingsystem | tr '[:upper:]' '[:lower:]')
+OS_RELEASE=$(facter lsbdistrelease | tr '[:upper:]' '[:lower:]')
+if [[ "$OS_RELEASE" == "8" && "$OS" == 'centos' ]]; then
+    # Setup the docker environment for jenkins user
+    docker build -f ${WORKSPACE}/Dockerfile \
+          --build-arg SIGN_DIR=${SIGN_DIR} \
+          -t sigul-sign .
+
+    docker volume create --driver local \
+      --opt type=none \
+      --opt device=/w/workspace \
+      --opt o=bind \
+      wrkspc_vol
+
+    docker volume inspect wrkspc_vol
+
+    docker run -e SIGUL_KEY="${SIGUL_KEY}" -e SIGUL_PASSWORD="${SIGUL_PASSWORD}" \
+              -e SIGUL_CONFIG=${SIGUL_CONFIG} \
+              -e SIGN_DIR=${SIGN_DIR} \
+              -e WORKSPACE=${WORKSPACE} \
+              --name sigul-sign \
+	            --security-opt label:disable \
+              --mount type=bind,source="/w/workspace",target="/w/workspace" \
+              --mount type=bind,source="/home/jenkins",target="/home/jenkins" \
+              -u root:root -w $(pwd) sigul-sign
+else
+    lftools sign sigul -m "${SIGN_MODE}" "${SIGN_DIR}"
+fi
