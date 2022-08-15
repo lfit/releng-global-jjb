b644c5aaf17e86a0ea013bf1ecdb86b6b8e30408
Jessica Wagantall <jwagantall@linuxfoundation.org>
2022-08-15 12:30:10 -0700

    Fix: Add CLI Sonar scanner job
    Introduce a SonarScanner CLI based job for Python
    based projects.
    This job will call the CLI directly instead of calling
    the maven sonar goal.
    
    Issue: RELENG-4427
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>
    Change-Id: If02a2c78bddacdcc273fb5a0b9f60b99d2da221d
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

---
+++ shell/sonar-cli.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2022 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+# Non-Maven Sonar CLI
+echo "---> sonar-cli.sh"
+
+SOURCE="binaries.sonarsource.com"
+DIRECTORY="/Distribution/sonar-scanner-cli"
+SCANNER_BIN="sonar-scanner-${SONAR_SCANNER_VERSION}-linux"
+
+export SONAR_SCANNER_HOME=${WORKSPACE}/.sonar/${SCANNER_BIN}
+curl --create-dirs -sSLo ${WORKSPACE}/.sonar/sonar-scanner.zip \
+    https://${SOURCE}/${DIRECTORY}/${SCANNER_BIN}.zip
+unzip -o ${WORKSPACE}/.sonar/sonar-scanner.zip -d ${WORKSPACE}/.sonar/
+export PATH=${SONAR_SCANNER_HOME}/bin:$PATH
+export ${SONAR_SCANNER_OPTS}
+export ${SONAR_TOKEN}
+
+echo "Running sonar-scanner"
+sonar-scanner \
+    -Dsonar.organization=${SONARCLOUD_PROJECT_ORGANIZATION} \
+    -Dsonar.projectKey=${SONARCLOUD_PROJECT_ORGANIZATION}_${SONARCLOUD_PROJECT_KEY} \
+    -Dsonar.sources=. \
+    -Dsonar.host.url=https://sonarcloud.io
