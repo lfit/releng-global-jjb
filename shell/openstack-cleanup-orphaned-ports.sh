ecfa0590bf35e30954bd66c1cf3ff2313606785b
Matthew Watkins <mwatkins@linuxfoundation.org>
2022-09-06 16:03:30 +0100

    Fix: correct parsing of openstack ports info
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>
    Change-Id: I9801a1ac0d584f2369cf77ed94acc5ce6a3c5b14

--- shell/openstack-cleanup-orphaned-ports.sh
+++ shell/openstack-cleanup-orphaned-ports.sh
@@ -28,6 +28,35 @@ if [ ${#os_ports_ts[@]} -eq 0 ]; then
 else
     cutoff=$(date -d "30 minutes ago"  +%s)
     for port_ts in "${os_ports_ts[@]}"; do
+mapfile -t os_ports_ts < <(openstack --os-cloud "$os_cloud" port list \
+        -f value \
+        -c ID \
+        -c status \
+        | grep -E DOWN )
+
+if [ ${#os_ports_ts[@]} -eq 0 ]; then
+    echo "No orphaned ports found."
+else
+    cutoff=$(date -d "30 minutes ago"  +%s)
+    for port_ts in "${os_ports_ts[@]}"; do
+        port_uuid="${port_ts% *}"
+        echo "Retrieving timestamp for port uuid: ${port_uuid}"
+        created_at=`openstack port show fc8789b7-06dc-40a8-8739-f06d472896d3 -f value -c created_at`
+        created_at_uxts=$(date -d "${created_at}" +"%s")
+        # Validate timestamp is numeric value
+        if [[ "$created_at_uxts" -eq "$created_at_uxts" ]]; then
+                # Clean up ports where created_at > 30 minutes
+                if [[ "$created_at_uxts" -gt "$cutoff" ]]; then
+                        echo "Removing orphaned port $port_uuid created_at ts > 30 minutes."
+                        openstack --os-cloud "$os_cloud" port delete "$port_uuid"
+                else
+                        echo "Port did not meet requirement for deletion; moving on"
+                fi
+        else
+                echo "Date variable failed numeric test; deletion not possible"
+        fi
+    done
+fi
         created_at_isots="${port_ts#* }"
         port_uuid="${port_ts% *}"
         echo "checking port uuid: ${port_uuid} with TS: ${created_at_isots}"
