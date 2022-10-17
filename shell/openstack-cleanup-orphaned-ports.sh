d7a4f7e82a72852281f69d2524d2deab2e0c1408
Matthew Watkins <mwatkins@linuxfoundation.org>
2022-10-17 21:55:55 +0100

    Fix: Abort script earlier when no objects to cleanup
    Issue-ID: RELENG-4483
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>
    Change-Id: I3311fc09a61f4d30fff8dd52e387c11dbcdc2aea

--- shell/openstack-cleanup-orphaned-ports.sh
+++ shell/openstack-cleanup-orphaned-ports.sh
@@ -60,6 +60,13 @@ openstack --os-cloud "$os_cloud" port list -f value -c ID -c status \
 
 # Count the number to process
 total=$(wc -l "$tmpfile" | awk '{print $1}')
+
+if [ "$total" -eq 0 ]; then
+    echo "No orphaned ports to process."
+    rm "$tmpfile"
+    exit 0
+fi
+
 echo "Ports to process: $total; age limit: $cutoff"
 echo "Using $threads parallel processes..."
 
