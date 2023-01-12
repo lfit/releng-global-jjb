ebc867e7ff5d005e8ea2cc7d45de5164416541ff
Matthew Watkins <mwatkins@linuxfoundation.org>
2023-01-12 17:04:19 +0000

    Fix: Update Nexus IQ script output to include Python dependencies
    Script now downloads module dependencies into the target directory.
    Also, print a warning if the target variable is not set/populated and
    print the correct variable in the job output.
    
    Issue-ID: RELENG-4557
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>
    Change-Id: I1d4bbd752dd2282fc58c10636ad1aa228ad919a8

--- shell/nexus-iq-python-cli.sh
+++ shell/nexus-iq-python-cli.sh
@@ -17,13 +17,35 @@ echo "---> nexus-iq-python-cli.sh"
 set -eu
 # do not print commands, credentials should not be logged
 set +x
+
+# Use pyenv to select the latest python version
+if [[ -d "/opt/pyenv" ]]; then
+    echo "Setup pyenv:"
+    export PYENV_ROOT="/opt/pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    pyenv versions
+    if command -v pyenv 1>/dev/null 2>&1; then
+        eval "$(pyenv init - --no-rehash)"
+        # Choose the latest numeric Python version from installed list
+        version=$(pyenv versions --bare | sed '/^[^0-9]/d' | sort -V | tail -n 1)
+        pyenv local "${version}"
+    fi
+fi
+
 CLI_LOCATION="/tmp/nexus-iq-cli-${NEXUS_IQ_CLI_VERSION}.jar"
 echo "INFO: downloading nexus-iq-cli version $NEXUS_IQ_CLI_VERSION"
 wget -nv "https://download.sonatype.com/clm/scanner/nexus-iq-cli-${NEXUS_IQ_CLI_VERSION}.jar" -O "${CLI_LOCATION}"
 echo "-a" > cli-auth.txt
 echo "${NEXUS_IQ_USER}:${NEXUS_IQ_PASSWORD}" >> cli-auth.txt
-echo "INFO: running nexus-iq-cli on project $NEXUS_IQ_PROJECT_NAME and file $REQUIREMENTS_FILE"
+if [ -z "${NEXUS_TARGET_BUILD}" ]; then
+    echo "Warning: NEXUS_TARGET_BUILD has not been set"
+fi
+echo "INFO: running nexus-iq-cli on project $NEXUS_IQ_PROJECT_NAME and target: ${NEXUS_TARGET_BUILD}"
+echo "Downloading Python dependencies into target directory"
+pip3 download -r requirements.txt -d "${NEXUS_TARGET_BUILD}"
 # result.json is a mystery
+# Do NOT double-quote ${NEXUS_TARGET_BUILD} below; causes breakage
+# shellcheck disable=SC2086
 java -jar "${CLI_LOCATION}" @cli-auth.txt \
     -s https://nexus-iq.wl.linuxfoundation.org -i "${NEXUS_IQ_PROJECT_NAME}" \
     -t build -r result.json ${NEXUS_TARGET_BUILD}
