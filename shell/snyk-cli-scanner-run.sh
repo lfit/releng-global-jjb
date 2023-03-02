1842c2ba1b900063ef9b68a9b6ca86bc73efb716
Jessica Wagantall <jwagantall@linuxfoundation.org>
2023-03-01 18:30:13 -0800

    Feat: Add Maven Snyk CLI Scanner
    Introduce Maven Snyk CLI scanner jobs. These jobs can be triggered
    to download the latest version of Snyk's CLI scanner and trigger a
    scan for Maven based repos.
    
    These jobs produce a report which is published into Snyk's dashboard.
    These reports are fetched and reflected back into the LFX Security tool.
    
    Issue: RELENG-4609
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>
    Change-Id: I183bd1e8d22caa832b04b7d1d5b078b0a8946285

---
+++ shell/snyk-cli-scanner-run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2023 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> snyk-cli-scanner-run.sh"
+
+# Download and install the latest Snyk scanner
+echo "Installing Snyk (latest)..."
+curl https://static.snyk.io/cli/latest/snyk-linux -o snyk
+sudo chmod +x ./snyk
+sudo mv ./snyk /usr/local/bin/
+echo "Verifying Snyk version..."
+snyk --version
+echo "Authenticate with SNYK_TOKEN..."
+snyk auth "$SNYK_CLI"
+echo "Running Snyk CLI..."
+snyk test --json --severity-threshold=low --org="$SNYK_ORG"
+snyk monitor --severity-threshold=low --org="$SNYK_ORG"
