93372c6225b26770b6d36aebf6dd2c78cb27f308
Bengt Thuree <bthuree@linuxfoundation.org>
2023-03-09 20:50:41 +1100

    FIX: Revert clm-maven plugin
    Latest version of clm-maven plugin (clm-maven-plugin:2.42.0-01) is not working in
    our environment.
    We get the following error
    00:03:00.118 [ERROR] Failed to execute goal com.sonatype.clm:clm-maven-plugin:2.42.0-01:index (default-cli) on project babel: Failed to invoke Maven build. Maven execution failed, exit code: 1 -> [Help 1]
    
    This patch pins the previous working plugin which is 2.41.0-02
    
    Issue-ID: IT-25225
    Change-Id: I3d87d5adc954baf703f29816c0e295e0203b4f40
    Signed-off-by: Bengt Thuree <bthuree@linuxfoundation.org>

--- shell/sonatype-clm.sh
+++ shell/sonatype-clm.sh
@@ -21,7 +21,7 @@ export MAVEN_OPTS
 
 # Disable SC2086 because we want to allow word splitting for $MAVEN_* parameters.
 # shellcheck disable=SC2086
-$MVN $MAVEN_GOALS dependency:tree com.sonatype.clm:clm-maven-plugin:index \
+$MVN $MAVEN_GOALS dependency:tree com.sonatype.clm:clm-maven-plugin:2.41.0-02:index \
     --global-settings "$GLOBAL_SETTINGS_FILE" \
     --settings "$SETTINGS_FILE" \
     -DaltDeploymentRepository=staging::default::file:"$WORKSPACE"/m2repo \
