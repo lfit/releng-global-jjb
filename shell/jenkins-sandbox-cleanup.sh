45d9a115f755b95b4fc25da7a25ed82bda4efe98
Anil Belur <abelur@linuxfoundation.org>
2023-06-13 17:58:16 +1000

    Fix: Pin urllib3 to <2.0.0 for the JJB cleanup
    Issue: RELENG-4715
    Change-Id: I8d5ef5e30682654b4212a320ffa20f061e11d828
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- shell/jenkins-sandbox-cleanup.sh
+++ shell/jenkins-sandbox-cleanup.sh
@@ -16,7 +16,8 @@ set -euf -o pipefail
 # shellcheck disable=SC1090
 . ~/lf-env.sh
 
-lf-activate-venv --python python3 --venv-file /tmp/.jjb_venv jenkins-job-builder
+lf-activate-venv --python python3 --venv-file /tmp/.jjb_venv \
+    jenkins-job-builder urllib3~=1.26.15
 
 # jenkins-jobs does not always open 'stdin' which may cause 'yes' to fail
 (yes || true) | jenkins-jobs -s sandbox delete-all
