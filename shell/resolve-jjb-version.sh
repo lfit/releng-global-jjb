4222826b7cd2d23251da2cc0a0d1ba6fefd9336d
Vratko Polak <vrpolak@cisco.com>
2024-04-03 17:37:03 +0200

    Feat: Allow reading jjb_version.txt
    This applies to ci jobs (those that use lf-infra-jjbini macro).
    The old behavior (explicit value via lf-infra-jjb-parameters)
    still works the same way as before.
    
    Ther new functionality is that project may pass "auto"
    as their value there.
    The script resolve-jjb-version.sh then attempts to read
    ${WORKSPACE}/jjb_version.txt
    and if that succeeds, it exports the value found there
    as the new JJB_VERSION.
    
    This new feature should gratly simplify
    major jjb version upgrades, as verify, deploy and merge jobs
    previously configured by old jjb version
    will use the new value in their run.
    
    Issue: RELENG-5184
    Change-Id: I24c02ad1bb734d2694273e7c64b972d68a35acd1
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ shell/resolve-jjb-version.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2024 Cisco and/or its affiliates.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> resolve-jjb-version.sh"
+
+set -x
+
+if [[ "${OPTIONAL_JJB_VERSION:-auto}" == "auto" ]]
+then
+    { tmp_version=$(< ${WORKSPACE}/jjb_version.txt); } 2> /dev/null
+    if [[ "${tmp_version}" != "" ]]
+    then
+        OPTIONAL_JJB_VERSION="${tmp_version}"
+    fi
+fi
+if [[ "${OPTIONAL_JJB_VERSION:-auto}" == "auto" ]]
+then
+    OPTIONAL_JJB_VERSION="6.0.0"
+fi
+
+if echo "JJB_VERSION=${OPTIONAL_JJB_VERSION}" > "${WORKSPACE}/__jjb_version.txt"
+then
+    cat "${WORKSPACE}/__jjb_version.txt"
+else
+    echo "Failed to resolve JJB_VERSION!"
+fi
