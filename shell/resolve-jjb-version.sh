66c180a817789da61206f5c5dbc33a04cc0d5169
Vratko Polak <vrpolak@cisco.com>
2024-04-04 15:41:08 +0200

    Feat: Allow reading jjb_version.txt
    This applies to ci jobs (those that use lf-infra-jjbini macro).
    The old behavior (explicit value via lf-infra-jjb-parameters)
    still works the same way as before.
    
    Ther new functionality is that project may pass "auto"
    as their value there.
    The script resolve-jjb-version.sh then attempts to parse
    ${WORKSPACE}/requirements.txt
    and if that succeeds, it exports the value found there
    as the RESOLVED_JJB_VERSION.
    
    This new feature should greatly simplify
    major jjb version upgrades, as verify, deploy and merge jobs
    previously configured by old jjb version from last job update
    will use the new value in their run.
    Only these three jobs currently use RESOLVED_JJB_VERSION,
    so other jobs that use lf-infra-jjbini should remain unaffected.
    
    As scripts for the three jobs need to perform lf-activate-venv
    differently (to use RESOLVED_JJB_VERSION instead of JJB_VERSION),
    they no longer source from ~/lf-env.sh script.
    
    Issue: RELENG-5184
    Change-Id: I24c02ad1bb734d2694273e7c64b972d68a35acd1
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ shell/resolve-jjb-version.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2024 Cisco and/or its affiliates.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> resolve-jjb-version.sh"
+
+RESOLVED_JJB_VERSION="${JJB_VERSION:-auto}"
+if [[ "${RESOLVED_JJB_VERSION}" == "auto" ]]
+then
+    echo "Looking into requirements.txt for JJB version."
+    version_line=$(grep '^jenkins-job-builder==' "${WORKSPACE}/requirements.txt")
+    version_substring=$(echo "${version_line}" | cut -d '=' -f 3)
+    # There could be a trailing comment.
+    version_value=$(echo "${version_substring}" | cut -d ' ' -f 1 | cut -d '#' -f 1)
+    if [[ "${tmp_version}" != "" ]]
+    then
+        echo "Found: ${tmp_version}"
+        RESOLVED_JJB_VERSION="${tmp_version}"
+    else
+        echo "JJB version or requirements.txt not found."
+    fi
+else
+    echo "Not asked to autodetect, using provided value: ${RESOLVED_JJB_VERSION}"
+fi
+if [[ "${RESOLVED_JJB_VERSION}" == "auto" ]]
+then
+    echo "Falling back to default JJB version."
+    RESOLVED_JJB_VERSION="6.0.0"
+fi
+
+if echo "RESOLVED_JJB_VERSION=${RESOLVED_JJB_VERSION}" > "${WORKSPACE}/_resolved_jjb_version.txt"
+then
+    cat "${WORKSPACE}/_resolved_jjb_version.txt"
+else
+    echo "Failed to save JJB version!"
+fi
