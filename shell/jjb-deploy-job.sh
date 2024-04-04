e1a26164c7e429368daf68522022ee99eef9f061
Vratko Polak <vrpolak@cisco.com>
2024-04-04 13:36:50 +0200

    Feat: Allow reading jjb_version.txt
    This applies to ci jobs (those that use lf-infra-jjbini macro).
    The old behavior (explicit value via lf-infra-jjb-parameters)
    still works the same way as before.
    
    Ther new functionality is that project may pass "auto"
    as their value there.
    The script resolve-jjb-version.sh then attempts to read
    ${WORKSPACE}/jjb_version.txt
    and if that succeeds, it exports the value found there
    as the RESOLVED_JJB_VERSION.
    
    This new feature should greatly simplify
    major jjb version upgrades, as verify, deploy and merge jobs
    previously configured by old jjb version from last job update
    will use the new value in their run.
    Only these three jobs currently use RESOLVED_JJB_VERSION,
    so other jobs that use lf-infra-jjbini should remain unaffected.
    
    Issue: RELENG-5184
    Change-Id: I24c02ad1bb734d2694273e7c64b972d68a35acd1
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- shell/jjb-deploy-job.sh
+++ shell/jjb-deploy-job.sh
@@ -16,8 +16,123 @@ echo "---> jjb-deploy-job.sh"
 # Ensure we fail the job if any steps fail.
 set -uef -o pipefail
 
-# shellcheck disable=SC1090
-source ~/lf-env.sh
+# Compared to lf-env.sh, this looks at RESOLVED_JJB_VERSION.
+lf-activate-venv () {
+    lf_venv=$(mktemp -d /tmp/venv-XXXX)
+    local venv_file="/tmp/.os_lf_venv"
+    local python=python3
+    local options
+    local set_path=true
+    local install_args=""
+    # set -x
+    options=$(getopt -o 'np:v:' -l 'no-path,system-site-packages,python:,venv-file:' \
+                -n "${FUNCNAME[0]}" -- "$@" )
+    eval set -- "$options"
+    while true; do
+        case $1 in
+            -n|--no-path) set_path=false ; shift   ;;
+            -p|--python)  python="$2"      ; shift 2 ;;
+            -v|--venv-file) venv_file="$2" ; shift 2 ;;
+            --system-site-packages) install_args="--system-site-packages" ;
+                                    shift ;;
+            --) shift; break ;;
+            *)  lf-echo-stderr \
+                "${FUNCNAME[0]}(): ERROR: Unknown switch '$1'." ;
+                return 1 ;;
+        esac
+    done
+
+    case $python in
+    python2*)
+        local pkg_list="$*"
+        # For Python2, just create venv and install pip
+        virtualenv -p "$python" "$lf_venv" || return 1
+        "$lf_venv/bin/pip" install --upgrade --quiet pip || return 1
+        if [[ -z $pkg_list ]]; then
+            echo "${FUNCNAME[0]}(): WARNING: No packages to install"
+            return 0
+        fi
+        echo "${FUNCNAME[0]}(): INFO: Installing: $pkg_list"
+        # $pkg_list is expected to be unquoted
+        # shellcheck disable=SC2086
+        "$lf_venv/bin/pip" install --upgrade --quiet $pkg_list || return 1
+        ;;
+    python3*)
+        local pkg_list=""
+        # Use pyenv for selecting the python version
+        if [[ -d "/opt/pyenv" ]]; then
+            echo "Setup pyenv:"
+            export PYENV_ROOT="/opt/pyenv"
+            export PATH="$PYENV_ROOT/bin:$PATH"
+            pyenv versions
+            if command -v pyenv 1>/dev/null 2>&1; then
+                eval "$(pyenv init - --no-rehash)"
+                # shellcheck disable=SC2046
+                pyenv local $(lf-pyver "${python}")
+            fi
+        fi
+
+        # Add version specifier for some packages
+        for arg in "$@"; do
+            case $arg in
+                jenkins-job-builder)
+                    pkg_list+="jenkins-job-builder==${RESOLVED_JJB_VERSION} " ;;
+                *)                   pkg_list+="$arg " ;;
+            esac
+        done
+
+        # Precedence:
+        # - Re-use venv:
+        #     1. --venv-file <path/to/file> as lf_venv
+        #     2. default: "/tmp/.os_lf_venv"
+        # - Create new venv when 1. and 2. is absent
+        if [[ -f "$venv_file" ]]; then
+            lf_venv=$(cat "$venv_file")
+            echo "${FUNCNAME[0]}(): INFO: Reuse venv:$lf_venv from" \
+                "file:$venv_file"
+        elif [[ ! -f "$venv_file" ]]; then
+            if [[ -n "$install_args" ]]; then
+                $python -m venv "$install_args" "$lf_venv" || return 1
+            else
+                $python -m venv "$lf_venv" || return 1
+            fi
+            echo "${FUNCNAME[0]}(): INFO: Creating $python venv at $lf_venv"
+            echo "$lf_venv" > "$venv_file"
+            echo "${FUNCNAME[0]}(): INFO: Save venv in file: $venv_file"
+        fi
+
+        "$lf_venv/bin/python3" -m pip install --upgrade --quiet pip \
+                        virtualenv || return 1
+        if [[ -z $pkg_list ]]; then
+            echo "${FUNCNAME[0]}(): WARNING: No packages to install"
+        else
+            echo "${FUNCNAME[0]}(): INFO: Installing: $pkg_list"
+            # $pkg_list is expected to be unquoted
+            # shellcheck disable=SC2086
+            "$lf_venv/bin/python3" -m pip install --upgrade --quiet \
+                        --upgrade-strategy eager $pkg_list || return 1
+        fi
+        ;;
+    *)
+        lf-echo-stderr "${FUNCNAME[0]}(): ERROR: No support for: $python"
+        return 1
+        ;;
+    esac
+
+    if ! type "$python" > /dev/null; then
+        lf-echo-stderr "${FUNCNAME[0]}(): ERROR: Unknown Python: $python"
+        return 1
+    fi
+
+    if $set_path; then
+        echo "${FUNCNAME[0]}(): INFO: Adding $lf_venv/bin to PATH"
+        PATH=$lf_venv/bin:$PATH
+        return 0
+    else
+        echo "${FUNCNAME[0]}(): INFO: Path not set, lf_venv set to: $lf_venv"
+    fi
+
+}   # End lf-activate-venv
 
 # Version controlled by JJB_VERSION
 lf-activate-venv --python python3 --venv-file /tmp/.jjb_venv jenkins-job-builder setuptools==65.7.0
---
+++ shell/resolve-jjb-version.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2024 Cisco and/or its affiliates.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> resolve-jjb-version.sh"
+
+RESOLVED_JJB_VERSION="${JJB_VERSION:-auto}"
+if [[ "${RESOLVED_JJB_VERSION}" == "auto" ]]
+then
+    { tmp_version=$(< "${WORKSPACE}/jjb_version.txt"); } 2> /dev/null
+    if [[ "${tmp_version}" != "" ]]
+    then
+        RESOLVED_JJB_VERSION="${tmp_version}"
+    fi
+fi
+if [[ "${RESOLVED_JJB_VERSION}" == "auto" ]]
+then
+    RESOLVED_JJB_VERSION="6.0.0"
+fi
+
+if echo "RESOLVED_JJB_VERSION=${RESOLVED_JJB_VERSION}" > "${WORKSPACE}/_resolved_jjb_version.txt"
+then
+    cat "${WORKSPACE}/_resolved_jjb_version.txt"
+else
+    echo "Failed to resolve JJB version!"
+fi
