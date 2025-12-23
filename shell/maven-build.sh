38987c0fe7098920434580288a49bb71945db517
Kevin Sandi <ksandi@contractor.linuxfoundation.org>
2025-12-22 21:07:39 -0600

    Fix: Add backward compatibility for maven-deploy-plugin
    Issue-ID: IT-29089
    Signed-off-by: Kevin Sandi <ksandi@contractor.linuxfoundation.org>
    Change-Id: I6c16cec0391147ee273f2d77adc8e344b0683960

--- shell/maven-build.sh
+++ shell/maven-build.sh
@@ -17,13 +17,33 @@ echo "---> maven-build.sh"
 set -xe -o pipefail
 set +u
 
+# Determine deployment repository format based on maven-deploy-plugin version
+# Version 3+ uses simplified format without ::default::
+plugin_version=$($MVN help:describe -Dplugin=org.apache.maven.plugins:maven-deploy-plugin -Ddetail \
+    --global-settings "$GLOBAL_SETTINGS_FILE" 2>/dev/null \
+    | grep "^Version:" | awk '{print $2}' || echo "2.8.2")
+
+if [[ "$plugin_version" -lt "3" ]]; then
+  # Disable SC2016 because we don't want $WORKSPACE to be expanded here.
+  # shellcheck disable=SC2016
+  alt_repo='-DaltDeploymentRepository=staging::default::file:"$WORKSPACE"/m2repo'
+else
+  # Disable SC2016 because we don't want $WORKSPACE to be expanded here.
+  # shellcheck disable=SC2016
+  # Disable SC2089 because the embedded quotes are intentional.
+  # shellcheck disable=SC2089
+  alt_repo='-DaltDeploymentRepository=staging::file:"$WORKSPACE"/m2repo'
+fi
+
 export MAVEN_OPTS
 
 # Disable SC2086 because we want to allow word splitting for $MAVEN_* parameters.
 # shellcheck disable=SC2086
+# Disable SC2090 because the embedded quotes are intentional.
+# shellcheck disable=SC2090
 $MVN $MAVEN_GOALS \
     -e \
     --global-settings "$GLOBAL_SETTINGS_FILE" \
     --settings "$SETTINGS_FILE" \
-    -DaltDeploymentRepository=staging::file:"$WORKSPACE"/m2repo \
+    $alt_repo \
     $MAVEN_OPTIONS $MAVEN_PARAMS
